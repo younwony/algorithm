2198e45223ef33ddd0829515d184848ba2292f56
younwony <wony9324@naver.com>
2019-09-18 15:25:40 +0900

    BOJ 1000~1002 complete

---
+++ BOJ/src/Java/Main.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class Main {
+
+	/**
+	 * @사용처 제출용 Class
+	 * @Todo
+	 * @param args
+	 * TODO
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
---
+++ BOJ/src/Java/a1003.java
@@ -0,0 +1,30 @@
+/**
+ * @작성자 wony
+ * @작성일 2019. 9. 18.
+ * @사용처
+ * TODO
+ */
+package Java;
+
+/**
+ * @작성자 wony
+ * @작성일 2019. 9. 18.
+ * @사용처
+ * TODO
+ */
+public class a1003 {
+
+	/**
+	 * @작성자 wony
+	 * @작성일 2019. 9. 18.
+	 * @사용처
+	 * @Todo
+	 * @param args
+	 * TODO
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
