2d0b26e4661d990a4036cf546158ec61a68b56dd
younwony <wony9324@naver.com>
2019-10-20 19:08:55 +0900

    단계별 문제 -> 함수 -> a2447번까지 풀이완료, 2447번 분할정복,재귀 공부 더필요(재풀이)

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -11,24 +11,48 @@ public class Main {
 	 * @param args
 	 * TODO
 	 */
+
+	 static char a[][];
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		
-		int[] result = new int[9];
-		int max = 0;
-		int maxIndex = 0;
-		
-		for(int i = 0; i < 9; i ++){
-			result[i] = scanner.nextInt();
-			if(result[i] > max){
-				max = result[i];
-				maxIndex = i+1;
-			}
-		}
-		
-		System.out.println(max);
-		System.out.println(maxIndex);
-		System.out.println("test");
-	}
+		 Scanner sc = new Scanner(System.in);
+	        
+	        int n = sc.nextInt();
+	        
+	        a = new char[n][n];
+	        for(int i=0; i<n; i++) {
+	            for(int j=0; j<n; j++) {
+	                a[i][j] = '*';
+	            }
+	        }
+	        
+	        recursive(0, 0, n);
+	        StringBuilder sb = new StringBuilder();
+	        for(int i=0; i<n; i++) {
+	            for(int j=0; j<n; j++) {
+	                sb.append(a[i][j]);
+	            }
+	            sb.append("\n");
+	        }
+	        System.out.println(sb.toString());
+	    }
+	    
+	    public static void recursive(int s, int e, int len) {
+	        if(len < 3) return;
+	        int m = len/3;
+	        for(int i=0; i<3; i++) {
+	            for(int j=0; j<3; j++) {
+	                if(i == 1 && j == 1) fillBlank(s + m, e + m, m);
+	                else recursive(s + m*i, e + m*j, m);
+	            }
+	        }
+	    }
+	    
+	    public static void fillBlank(int s, int e, int len) {
+	        for(int i=s; i<s+len; i++) {
+	            for(int j=e; j<e+len; j++) {
+	                a[i][j] = ' ';
+	            }
+	        }
+	    }
 }
