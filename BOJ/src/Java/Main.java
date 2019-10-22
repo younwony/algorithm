4c391ba33e6559af7338d5d77049266b6d6ce13d
younwony <wony9324@naver.com>
2019-10-22 17:28:35 +0900

    Git Test

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -12,67 +12,47 @@ public class Main {
 	 * TODO
 	 */
 
-	static char[][] star;
+	 static char a[][];
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in); 
-		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int testCount = scanner.nextInt();
-		
-		star = new char[testCount][testCount];
-
-		star(testCount, 0, 0);
-		
-		try {
-			for(int i=0; i<testCount; i++){
-				for(int j=0; j<testCount; j++){
-					bufferedWriter.write(star[i][j]);
-				}
-				bufferedWriter.newLine();
-			}
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}finally{
-			try {
-				if(bufferedWriter != null) {
-					bufferedWriter.flush();
-					bufferedWriter.close();
-				}
-			} catch (Exception e2) {
-				// TODO: handle exception
-				e2.printStackTrace();
-			}
-		}
-		
-	}
-	
-	public static void star(int n, int x, int y){
-		if(n ==1){
-			star[x][y] = '*';
-			return;
-		}
-		
-		for(int i=0; i<3; i++){
-			for(int j=0; j<3; j++){
-				if(i==1 && j==1){
-					starBlank(n/3, x+n/3*i, y+n/3*j);
-				}else{
-					star(n/3, x+n/3*i, y+n/3*j);
-				}
-			}
-		}
-	}
-	
-	public static void starBlank(int n, int x, int y){
-		for(int i=0; i<n; i++){
-			for(int j=0; j<n; j++){
-				star[x+i][y+j] = ' ';
-			}
-		}
-	}
-	
+		 Scanner sc = new Scanner(System.in);
+	        
+	        int n = sc.nextInt();
+	        
+	        a = new char[n][n];
+	        for(int i=0; i<n; i++) {
+	            for(int j=0; j<n; j++) {
+	                a[i][j] = '*';
+	            }
+	        }
+	        
+	        recursive(0, 0, n);
+	        StringBuilder sb = new StringBuilder();
+	        for(int i=0; i<n; i++) {
+	            for(int j=0; j<n; j++) {
+	                sb.append(a[i][j]);
+	            }
+	            sb.append("\n");
+	        }
+	        System.out.println(sb.toString());
+	    }
+	    
+	    public static void recursive(int s, int e, int len) {
+	        if(len < 3) return;
+	        int m = len/3;
+	        for(int i=0; i<3; i++) {
+	            for(int j=0; j<3; j++) {
+	                if(i == 1 && j == 1) fillBlank(s + m, e + m, m);
+	                else recursive(s + m*i, e + m*j, m);
+	            }
+	        }
+	    }
+	    
+	    public static void fillBlank(int s, int e, int len) {
+	        for(int i=s; i<s+len; i++) {
+	            for(int j=e; j<e+len; j++) {
+	                a[i][j] = ' ';
+	            }
+	        }
+	    }
 }
