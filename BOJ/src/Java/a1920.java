be4f4febf7e265ab57fa04bdf24c05700e5730c0
younwony <wony9324@naver.com>
2019-10-30 21:25:52 +0900

    책페이지(메모리제한)

---
+++ BOJ/src/Java/a1920.java
@@ -0,0 +1,32 @@
+/**
+ * @작성자 wony
+ * @작성일 2019. 10. 30.
+ * @사용처
+ * @Todo
+ */
+
+package Java;
+
+/**
+ * @작성자 wony
+ * @작성일 2019. 10. 30.
+ * @사용처
+ * @Todo
+ */
+
+public class a1920 {
+
+	/**
+	 * @작성자 wony
+	 * @작성일 2019. 10. 30.
+	 * @사용처
+	 * @param args
+	 * @Todo
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
