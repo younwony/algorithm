affe4feb51e2eba13be59733109a6eb8638ff58d
younwony <wony9324@naver.com>
2019-11-17 21:24:31 +0900

    1-4 기초 자료구조 (2) B 스택수열, C 조세퍼스 완료

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -8,52 +8,33 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+		Scanner scanner = new Scanner(System.in);
 		
-		try {
-			int[] memPlusResult = new int[10000001];
-			int[] memMinusResult = new int[10000001];
-			
-			Arrays.fill(memPlusResult, 0);
-			Arrays.fill(memMinusResult, 0);
-			
-			int inputCount = Integer.parseInt(bufferedReader.readLine());
-			
-			String[] inputArray = bufferedReader.readLine().split(" ");
-			
-			int outputCount = Integer.parseInt(bufferedReader.readLine());
-			
-			String[] outputArray = bufferedReader.readLine().split(" ");
-			
-			
-			for(int i = 0; i < inputCount; i++){
-				if(Integer.parseInt(inputArray[i]) >= 0) 
-					memPlusResult[Integer.parseInt(inputArray[i])]++; 
-				else
-					memMinusResult[Integer.parseInt(inputArray[i]) * -1]++;
-			}
-			
-			for(String data : outputArray){
-				if(Integer.parseInt(data) >= 0)
-					bufferedWriter.write(String.valueOf(memPlusResult[Integer.parseInt(data)]));
-				else
-					bufferedWriter.write(String.valueOf(memMinusResult[Integer.parseInt(data) * -1]));
-				bufferedWriter.write(" ");
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		} finally{
-			try {
-				if(bufferedReader != null) bufferedReader.close();
-				if(bufferedWriter != null){ bufferedWriter.flush(); bufferedWriter.flush();}
-				
-			} catch (Exception e2) {
-				// TODO: handle exception
-				e2.printStackTrace();
-			}
+		int N = scanner.nextInt(); // 인원
+		
+		int K = scanner.nextInt(); // K순서
+		
+		scanner.close();
+		
+		Deque<Integer> deque = new ArrayDeque<Integer>(); 
+		
+		for(int i = 1; i <= N; i++)
+			deque.offer(i);
+		
+		StringBuilder builder = new StringBuilder();
+		
+		builder.append("<");
+		
+		while(deque.size() != 0) {
+			for(int i = 1; i < K; i++) 
+				deque.offer(deque.poll());
+			builder.append(deque.poll() + ", ");
 		}
+		
+		builder.replace(builder.length()-2, builder.length(),"");
+		
+		builder.append(">");
+		
+		System.out.println(builder.toString());
 	}
-
 }
