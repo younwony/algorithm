cca61e17340db4d630b9a0e364e9c1be55c20510
younwony <wony9324@naver.com>
2019-11-23 16:09:55 +0900

    1-4 기초 자료구조 완료(AC 문제 완료)

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -9,52 +9,47 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		 
-		int testCount = scanner.nextInt();
 		
-		int documentSize, count, resultLocation;
+		int testCount = Integer.parseInt(scanner.nextLine());
 		
-		for(int i = 0; i< testCount; i++){
-			Queue<Integer> queue = new LinkedList<Integer>();
-			documentSize = scanner.nextInt();
-			resultLocation = scanner.nextInt();
-			for(int j = 0; j < documentSize; j++)
-				queue.offer(scanner.nextInt());
+		for(int i = 0; i < testCount; i++) {
 			
-			count = 0;
+			String[] order = scanner.nextLine().split("");
+			int testDataCount = Integer.parseInt(scanner.nextLine());
+			String data = scanner.nextLine();
+			String[] dataArray = data.substring(1, data.length()-1).split(",");
+			Deque<Integer> deque = new ArrayDeque<Integer>();
+			boolean reverse = false;
 			
-			while(resultLocation != -1){
-				if(queueGreateThenData(queue)){
-					if(resultLocation == 0) resultLocation += queue.size();
-					else{
-						queue.offer(queue.poll());
-						resultLocation--;
-					}
-				}else{
-					count++;
-					if(resultLocation == 0){
-						System.out.println(count);
-					}else{
-						queue.poll();
-					}
-					resultLocation--;
+			for(int j = 0; j < testDataCount; j++)
+				deque.offer(Integer.parseInt(dataArray[j]));
+			int k = 0;
+			int oL = order.length;
+			for(k = 0; k < oL; k++) {
+				if("R".equals(order[k])) reverse = !reverse;
+				if("D".equals(order[k])) {
+					if(deque.isEmpty())	break;
+					if(reverse) deque.pollLast();
+					else deque.poll();
 				}
 			}
 			
+			if(k < oL) System.out.println("error");
+			else {
+				StringBuilder builder = new StringBuilder();
+				String result;
+				builder.append("[");
+				while(!deque.isEmpty()) {
+					if(reverse)	builder.append(deque.pollLast() + ",");
+					else builder.append(deque.poll() + ",");
+				}
+				if(builder.length() > 1) result = builder.substring(0,builder.length()-1);
+				else result = builder.toString();
+				result += "]";
+				System.out.println(result);
+			}
 		}
+		
 		scanner.close();
-	}
-	
-	public static boolean queueGreateThenData(Queue<Integer> queue){
-		
-		int data = queue.peek();
-		
-		while(data != 9){
-			data++;
-			if(queue.contains(data))
-				return true;
-		}
-		
-		return false;
 	}
 }
