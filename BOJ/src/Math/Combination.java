78c5f41aea8828dbbef634bc7ee3a69d1dc13491
younwony <wony9324@naver.com>
2019-12-03 17:07:52 +0900

    조합 알고리즘 구현

--- BOJ/src/Math/Combination.java
+++ BOJ/src/Math/Combination.java
@@ -27,40 +27,67 @@ public class Combination {
 	 * @param args
 	 * 
 	 */
-	static int[] arr = new int[]{1,2,3,4};
-	static List<String> list = new ArrayList<String>();
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] result = new int[2];
-		doCombination(arr.length,result,2);
+		
+		int[] array = new int[]{1,2,3,4,5};
+		int n = array.length;
+		int r = 2;
+		int[] saveArray = new int[r];
+		
+		Combination(array, saveArray, 0, 0, n, r);
 		
 	}
 	
-	public static void doCombination(int n , int[] result, int r){
-		
-		if(r == 0){
-			print(result);
-		}else if(n == r){
-			for(int i = 0;  i < r; i++)
-				result[result.length - i] = arr[arr.length - i];
+	/**
+	 * @작성자 wony
+	 * @작성일 2019. 12. 3.
+	 * @사용처
+	 * @Todo
+	 * @param readArray : 주어진 n개의 배열
+	 * @param saveArray : 저장할 r개의 배열
+	 * @param readIndex : 주어진 N개의 배열중 읽을 위치를 알려주는 Index
+	 * @param saveIndex : 저장할 배열 중 저장할 위치를 알려주는 Index
+	 * @param n : n개의 원소
+	 * @param r : r개의 선택
+	 * 
+	 */
+	public static void Combination(int[] readArray, int[] saveArray, int readIndex, int saveIndex, int n, int r){
+		if(r == 0) printArray(saveArray);		
+		else if(n == r){
+			/* 
+			 * n이 r과 같을경우 
+			 * 
+			 * r의 개수만큼 현재 index에서 하나씩 뽑아 넣어준다.
+			 * */
+			for(int i = 0; i < r; i++) saveArray[saveIndex++] = readArray[readIndex++];
+			
+			printArray(saveArray);
 		}
-		
-		result[result.length - r] = arr[arr.length - n];
-		doCombination(n-1,result, r);
-		doCombination(n-1,result, r-1);
+		else{
+			//한개의 원소를 저장 후 , nCr = n-1Cr-1 + n-1Cr 실행
+			saveArray[saveIndex] = readArray[readIndex];
+			// n-1 C r -1
+			Combination(readArray, saveArray, readIndex + 1, saveIndex + 1, n - 1, r - 1);
+			// n-1 C r
+			Combination(readArray, saveArray, readIndex + 1, saveIndex, n - 1, r);
+			
+		}
 	}
 	
-	public static void print(int[] result){
-		String str = "";
-		for(int i = 0 ; i < result.length; i++)
-			str += result[i];
-		
-		System.out.println(str);
-	}
-	
-	public static void printeq(int n , int result, int r){
-		
+	/**
+	 * @작성자 wony
+	 * @작성일 2019. 12. 3.
+	 * @사용처 배열 출력
+	 * @Todo
+	 * @param array
+	 * 
+	 */
+	public static void printArray(int[] array){
+		for(int data : array)
+			System.out.print(data + " ");
+		System.out.println();
 	}
 }
