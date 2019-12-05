709d043d15cf311462961e60ede5057748c28b56
younwony <wony9324@naver.com>
2019-12-05 17:33:58 +0900

    BOJ 1783 Solved and 2816 ~ing

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -8,19 +8,25 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		 
-		 int n = scanner.nextInt();
-		 int m = scanner.nextInt();
-		 int k = scanner.nextInt();
-		 
-		 scanner.close();
-		 
-		 while(k != 0){
-			 if((double)n/2 >= m) n--;
-			 else m--;
-			 k--;
-		 }
-		 
-		 System.out.println(n/2 > m ? m : n/2);
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		scanner.close();
+		int count = 0;
+		if(n == 1)
+			count = 1;
+		else if(n == 2){
+			if(m <= 6){
+				count = (m+1)/2;
+			}else
+				count = 4;
+		}else{
+			if(m <= 4){
+				count = m;
+			}else if(m <= 6){
+				count = 4;
+			}else
+				count = m-2;
+		}
+		System.out.println(count);
 	}
 }
