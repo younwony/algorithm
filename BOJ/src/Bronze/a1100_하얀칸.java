d5479f0f66c57fee44d46c404405110c43b7e7d3
younwony <wony9324@naver.com>
2019-12-12 16:22:13 +0900

    BOJ Bronze 1100 Solved

---
+++ BOJ/src/Bronze/a1100_하얀칸.java
@@ -0,0 +1,47 @@
+/**
+ * @작성자 wony
+ * @작성일 2019. 12. 12.
+ * @사용처
+ *
+ */
+package Bronze;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import sun.util.locale.StringTokenIterator;
+
+/**
+ * @작성자 wony
+ * @작성일 2019. 12. 12.
+ * @사용처
+ * 
+ */
+public class a1100_하얀칸 {
+
+	/**
+	 * @작성자 wony
+	 * @작성일 2019. 12. 12.
+	 * @사용처
+	 * @Todo
+	 * @param args
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		String xFiled;
+		int count = 0;
+		for(int i = 0; i < 8; i++){
+			xFiled = scanner.nextLine();
+			for(int j = 0; j < 8; j++){
+				if((i+j)%2 == 0 && "F".equals(String.valueOf(xFiled.charAt(j))))
+					count++;
+			}
+		}
+		scanner.close();
+		
+		System.out.println(count);
+	}
+
+}
