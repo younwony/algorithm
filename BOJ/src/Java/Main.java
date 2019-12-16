0be7503f4d5676f34b60c0a7263075d000709f54
younwony <wony9324@naver.com>
2019-12-16 11:06:46 +0900

    BOJ Bronze (11382, 14918, 1550, 15733, 15740, 15894, 15962, 15964, 16394, 16430, 5337, 5338, 5522) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -7,31 +7,7 @@ import java.util.*;
 public class Main {
 	
 	public static void main(String[] args) {
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		try {
-			String text = bufferedReader.readLine();
-			int textLength = text.length();
-			int textI = textLength/10;
-			
-			for(int i = 1; i <= textI; i++){
-				bufferedWriter.write(text.substring(10*(i-1), 10*i) + "\n");
-				bufferedWriter.flush();
-			}
-			bufferedWriter.write(text.substring(10*textI));
-			bufferedWriter.flush();
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}finally{
-			try {
-				if(bufferedReader != null) bufferedReader.close();
-				if(bufferedWriter != null)bufferedWriter.close();
-			} catch (Exception e2) {
-				// TODO: handle exception
-				e2.printStackTrace();
-			}
-		}
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(scanner.nextBigInteger().add(scanner.nextBigInteger()));
 	}
 }
