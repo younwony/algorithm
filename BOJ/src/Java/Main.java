88626a85bd7939d02cdded6c275997a766c093f2
younwony <wony9324@naver.com>
2019-12-18 17:57:58 +0900

    BOJ 단계별 문제 - 문자열 (1152, 2675, 5622, 2941)

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -8,9 +8,24 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		BigInteger a = scanner.nextBigInteger();
-		BigInteger b = scanner.nextBigInteger();
+		
+		String[] croatiaLang = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+		String inputData = scanner.nextLine();
+		
 		scanner.close();
-		System.out.println(a.multiply(b));
+		int count = 0;
+		while(!"".equals(inputData)){
+			for(int i = 0; i < croatiaLang.length; i++){
+				while(inputData.indexOf(croatiaLang[i]) >= 0){
+					inputData = inputData.replaceFirst(croatiaLang[i], ",");
+					count++;
+				}
+			}
+			inputData = inputData.replace(",","");
+			count += inputData.length();
+			break;
+		}
+		System.out.println(count);
 	}
+	
 }
