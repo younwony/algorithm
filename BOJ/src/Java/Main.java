4c99d42980e06e845639da43c76c3559a50fd7ae
younwony <wony9324@naver.com>
2019-12-20 17:57:58 +0900

    BOJ Silver 1018 Solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -9,26 +9,47 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		int testCount = scanner.nextInt();
-		int[][] person = new int[testCount][2];
-		for(int i = 0; i < testCount; i++){
-			person[i][0] = scanner.nextInt();
-			person[i][1] = scanner.nextInt();
-		}
+		String[][] filed = new String[scanner.nextInt()][scanner.nextInt()];
+		scanner.nextLine();
+		for(int i = 0; i < filed.length; i++)
+			filed[i] = scanner.nextLine().split("");
 		scanner.close();
+		int countMin = 0;
 		
-		int height;
-		int weight;
-		int rank;
-		for(int i = 0; i < testCount; i++){
-			rank = 1;
-			height = person[i][0];
-			weight = person[i][1];
-			for(int j = 0; j < testCount; j++){
-				if(i==j) continue;
-				if(height < person[j][0] && weight < person[j][1] )	rank++;
+		for(int i = 0; i < filed.length-7; i++){
+			for(int j = 0; j < filed[0].length -7; j++){
+				if(i ==0 && j ==0) countMin = filedCount(filed, i, j);
+				else{
+					countMin = countMin > filedCount(filed, i, j) ? filedCount(filed, i, j) : countMin;
+				}
 			}
-			System.out.print(rank + " ");
 		}
+		
+		System.out.println(countMin);
+		
+	}
+	public static int filedCount(String[][] filed, int startX, int startY){
+		int countOne = 0;
+		for(int i = startX; i < startX+8; i++){
+			for(int j = startY; j < startY+8; j++){
+				if((i+j) % 2 == 0){
+					if("B".equals(filed[i][j])) countOne++;
+				}else{
+					if("W".equals(filed[i][j])) countOne++;
+				}
+			}
+		}
+		int countTwo = 0;
+		for(int i = startX; i < startX+8; i++){
+			for(int j = startY; j < startY+8; j++){
+				if((i+j) % 2 != 0){
+					if("B".equals(filed[i][j])) countTwo++;
+				}else{
+					if("W".equals(filed[i][j])) countTwo++;
+				}
+			}
+		}
+		
+		return countOne > countTwo ? countTwo : countOne;
 	}
 }
