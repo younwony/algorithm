036b4311ad99b473b5b5e6c93272ff3812be6727
younwony <wony9324@naver.com>
2019-12-23 18:16:34 +0900

    BOJ Bronze (10250, 1193) Solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -9,32 +9,17 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		
-		int n = scanner.nextInt();
-		
-		scanner.close();
-		
-		int[] memN = new int[10001];
-		
-		memN[0] = 666;
-		
-		for(int i = 0; i < n; i++) 
-			memN[i+1] = SyomNumber(memN[i]);
-		
-		System.out.println(memN[n-1]);
-	}
-	
-	public static int SyomNumber(int n) {
-		
-		String startN;
-		boolean syomN = true;
-		while(syomN) {
-			n++;
-			startN = String.valueOf(n);
-			for(int i = 0; i < startN.length()-2; i++) 
-				if(startN.charAt(i) == '6' && startN.charAt(i+1) == '6' && startN.charAt(i+2) == '6') {syomN = false;}
+		int testCount = scanner.nextInt();
+		int h,w,n,floor;
+		String ho;
+		for(int i = 0; i < testCount; i++) {
+			h = scanner.nextInt();
+			w = scanner.nextInt();
+			n = scanner.nextInt();
+			floor = n%h == 0 ? h : n%h;
+			ho = (((n-1)/h)+1) < 10 ? "0" + (((n-1)/h)+1) : String.valueOf((((n-1)/h)+1));
+			System.out.println(floor + ho);
 		}
-		return n;
 	}
 	
 }
