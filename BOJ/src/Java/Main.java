38ff4f0c80af91b7ae0fb710bbb9a4aabcc84a18
younwony <wony9324@naver.com>
2019-12-26 11:21:42 +0900

    BOJ Silver (1978, 2581, 4848, 0920) Solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -9,29 +9,44 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
+		
 		int testCount = scanner.nextInt();
-		int x,y;
-		for(int i = 0; i < testCount; i++) {
-			x = scanner.nextInt();
-			y = scanner.nextInt();
-			SpaceMove(y - x);
-		}
-	}
-	
-	public static void SpaceMove(int n) {
-		for(int i = 1; ; i++) {
-			if(i > Math.sqrt(n)) {
-				if(n <= (Math.pow(i-1,2) + i-1)) {
-					System.out.println(2*(i-1));
-				}else {
-					System.out.println(2*i - 1);
-				}
-				break;
-			}else if(i == Math.sqrt(n)) {
-				System.out.println((int)(2*Math.sqrt(n) - 1));
-				break;
+		
+		int[] memDecimal = new int[10001];
+		
+		memDecimal[1] = 1;
+		for(int i = 2; i < Math.sqrt(memDecimal.length); i++){
+			for(int j = i*i; j < memDecimal.length; j += i){
+				memDecimal[j] = 1;
 			}
 		}
+		
+		int n,x,y;
+		
+		ArrayList<Integer> goldBachPartition = new ArrayList<Integer>();
+		
+		for(int i = 0 ; i < testCount; i++){
+			goldBachPartition.clear();
+			n = scanner.nextInt();
+			x = 0;
+			y = 0;
+			for(int j = 1; j < n; j++){
+				if(memDecimal[j] == 0){goldBachPartition.add(j);}
+			}
+			
+			for(int k = 0; k < goldBachPartition.size(); k++){
+				if(goldBachPartition.get(k) > (n/2)+1) break;
+				if(goldBachPartition.contains(n-goldBachPartition.get(k))){
+					x = goldBachPartition.get(k);
+					y = n-x;
+				} 
+			}
+			
+			if(x > y){System.out.println(y + " " + x);}
+			else {System.out.println(x + " " + y);}
+		}
+		
+		scanner.close();
 	}
 	
 }
