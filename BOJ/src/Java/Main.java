dd358375e46e1b47e0a8987f1b1a949638a9d11e
younwony <wony9324@naver.com>
2019-12-26 13:39:19 +0900

    BOJ Bronze (3009, 3053, 4153)(수학2 단계) Solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -9,44 +9,10 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		
-		int testCount = scanner.nextInt();
-		
-		int[] memDecimal = new int[10001];
-		
-		memDecimal[1] = 1;
-		for(int i = 2; i < Math.sqrt(memDecimal.length); i++){
-			for(int j = i*i; j < memDecimal.length; j += i){
-				memDecimal[j] = 1;
-			}
-		}
-		
-		int n,x,y;
-		
-		ArrayList<Integer> goldBachPartition = new ArrayList<Integer>();
-		
-		for(int i = 0 ; i < testCount; i++){
-			goldBachPartition.clear();
-			n = scanner.nextInt();
-			x = 0;
-			y = 0;
-			for(int j = 1; j < n; j++){
-				if(memDecimal[j] == 0){goldBachPartition.add(j);}
-			}
-			
-			for(int k = 0; k < goldBachPartition.size(); k++){
-				if(goldBachPartition.get(k) > (n/2)+1) break;
-				if(goldBachPartition.contains(n-goldBachPartition.get(k))){
-					x = goldBachPartition.get(k);
-					y = n-x;
-				} 
-			}
-			
-			if(x > y){System.out.println(y + " " + x);}
-			else {System.out.println(x + " " + y);}
-		}
-		
+		int r = scanner.nextInt();
 		scanner.close();
+		System.out.println(String.format("%.6f",r * r * Math.PI));
+		System.out.println(String.format("%.6f",r * r * 2.0));
 	}
 	
 }
