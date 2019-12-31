5c4fcf7dbc1b7b0739c2ce5e6034e24f7dba6ea6
younwony <wony9324@naver.com>
2019-12-31 15:40:14 +0900

    BOJ Bronze (5086), Silver (9663) Solved, *(9663) NQueen(Backtraking) 다른 유형확인!

--- BOJ/src/Silver/a9663_N_Queen.java
+++ BOJ/src/Silver/a9663_N_Queen.java
@@ -26,25 +26,34 @@ public class a9663_N_Queen {
 	 */
 	static int count = 0;
 	static int n;
-	static boolean[][] filed;
+	static int[] col;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		n = scanner.nextInt();
-		filed = new boolean[n][n];
+		col = new int[n];
+		NQueen(0, 0);
 		scanner.close();
+		System.out.println(count);
 		
 	}
 	
-	public static void NQueen(int index){
-		if(index == n){
+	public static void NQueen(int rowNum, int queenCount){
+		if(queenCount == n){
 			count++;
 		}else{
-			for(int i = index; i < n; i++){
-				for(int j = 0; j < n; j++){
-					filed[i][j] = true;
+			boolean[] check = new boolean[n]; // 형재 row의 위치값들 체크할 Boolean
+			for(int i = 0; i < rowNum; i++){ //이전  Queen들의 위치
+				check[col[i]] = true; // 이전 Queen의 위치 col값 제외
+				if(col[i] - (rowNum - i) >= 0){check[col[i]-(rowNum - i)] = true;} //이전 Queen과 현재 row의 row차이만큼 이전Queen 위치의 col에서 뺀값 제외 ,즉 거리의 차이만큼 뺀값 제외
+				if(col[i] + (rowNum - i) < n){check[col[i]+(rowNum - i)] = true;} //이전 Queen과 현재 row의 row차이만큼 이전Queen 위치의 col에서 더한값 제외,즉 거리의 차이만큼 더한값 제외
+			}
+			
+			for(int i = 0; i < n; i++){
+				if(check[i] != true){ //제외되지 않은 값들이 현재 row에서 Queen을 놓을 수 있는 위치이다.
+					col[rowNum] = i; //현재 row에  Queen 위치 저장
+					NQueen(rowNum+1, queenCount+1);
 				}
-				
 			}
 		}
 	}
