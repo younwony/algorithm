b264a4b524265426df120b62cd5ff8f10b6a5dbe
younwony <wony9324@naver.com>
2020-01-04 13:20:26 +0900

    BOJ Bronze (11283) Solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -7,21 +7,8 @@ import java.util.*;
 public class Main {
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		int[] coinType = new int[scanner.nextInt()];
-		int k = scanner.nextInt();
-		for(int i = 0; i < coinType.length; i++)
-			coinType[i] = scanner.nextInt();
+		Scanner scanner = new Scanner(System.in); 
+		System.out.println((int)scanner.nextLine().charAt(0) - 44031);
 		scanner.close();
-		
-		int count = 0;
-		for(int i = coinType.length-1; i >=0; i--){
-			if(k/coinType[i] != 0){
-				count += k/coinType[i];
-				k %= coinType[i];
-			}
-		}
-		System.out.println(count);
 	}
 }
