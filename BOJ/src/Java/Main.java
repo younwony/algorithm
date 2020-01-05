7427608a87094c7c889efe0181f5799c18783288
younwony <wony9324@naver.com>
2020-01-05 18:27:13 +0900

    BOJ Silver (9095) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -6,9 +6,29 @@ import java.util.*;
 
 public class Main {
 	
+	static int count = 0;
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in); 
-		System.out.println((int)scanner.nextLine().charAt(0) - 44031);
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		int testCase = scanner.nextInt();
+
+		for(int i = 0 ; i < testCase; i++) {
+			BackTracking(scanner.nextInt());
+			System.out.println(count);
+			count=0;
+		}
 		scanner.close();
+	}
+	
+	
+	public static void BackTracking(int n) {
+		if(n == 0) {
+			count++;
+		}else if(n > 0){
+			BackTracking(n-1);
+			BackTracking(n-2);
+			BackTracking(n-3);
+		}
 	}
 }
