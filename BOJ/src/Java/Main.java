edadf3b1e36c824b79e0b12488bfd5771e663e1f
younwony <wony9324@naver.com>
2020-01-07 18:27:20 +0900

    BOJ Bronze (11282) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -8,28 +8,6 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		int testCount = scanner.nextInt();
-		HashMap<Integer, Integer> map = new LinkedHashMap<Integer, Integer>();
-		
-		int start, end, nowTime;
-		int count = 0;
-		for(int i = 0; i < testCount; i++){
-			start = scanner.nextInt();
-			end = scanner.nextInt();
-			map.put(end, start);
-		}
-		scanner.close();
-		
-		ArrayList<Integer> endTime = new ArrayList<Integer>(map.keySet());
-		endTime.sort(null);
-		nowTime = 0;
-		for(int i = 0; i < endTime.size(); i++){
-			if(nowTime <= map.get(endTime.get(i))){
-				nowTime = endTime.get(i);
-				count++;
-			}
-		}
-		System.out.println(count);
+		System.out.println((char)(new Scanner(System.in).nextInt()+44031));
 	}
 }
