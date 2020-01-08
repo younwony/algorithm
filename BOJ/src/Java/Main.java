c93478e4dc78fbef795ab7206c1d4c564ff12407
younwony <wony9324@naver.com>
2020-01-08 15:33:31 +0900

    BOJ Gold (15686) Solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -7,70 +7,71 @@ import java.util.*;
 
 public class Main {
 	
+	static ArrayList<int[]> arrayList  = new ArrayList<int[]>();
+	static ArrayList<int[]> filedList = new ArrayList<int[]>();
+	static int[][] filed;
+	static boolean[] visitied;
+	static int m = 0;
+	static int chickenlength = 2500;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		int testCount = scanner.nextInt();
-		ArrayList<MettingRoom> arrayList = new ArrayList<MettingRoom>();
-		for(int i = 0; i< testCount; i++){
-			arrayList.add(new MettingRoom(scanner.nextInt(), scanner.nextInt()));
-		}
-		scanner.close();
-		Collections.sort(arrayList);
 		
-		int nowTime = 0;
-		int count = 0;
-		for(MettingRoom result : arrayList){
-			if(nowTime <= result.startTime){
-				count++;
-				nowTime = result.endTime; 
+		int n = scanner.nextInt();
+		m = scanner.nextInt();
+		filed = new int[n][n];
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				filed[i][j] = scanner.nextInt();
+				if(filed[i][j] == 2){arrayList.add(new int[]{i,j});}
 			}
 		}
-		System.out.println(count);
-	}
-}
-
-class MettingRoom implements Comparable<MettingRoom>{
-	int startTime;
-	int endTime;
-	
-	public MettingRoom(int startTime, int endTime) {
-		this.startTime = startTime;
-		this.endTime = endTime;
+		scanner.close();
+		visitied = new boolean[arrayList.size()];
+		
+		dfs(0, 0);
+		
+		System.out.println(chickenlength);
+		
 	}
 	
-	public int getStartTime() {
-		return startTime;
-	}
-
-	public void setStartTime(int startTime) {
-		this.startTime = startTime;
-	}
-
-	public int getEndTime() {
-		return endTime;
-	}
-
-	public void setEndTime(int endTime) {
-		this.endTime = endTime;
-	}
-
-	/**
-	 * @작성자 wony
-	 * @작성일 2020. 1. 8.
-	 * @사용처
-	 * @param o
-	 * @return
-	 * 
-	 */
-	@Override
-	public int compareTo(MettingRoom o) {
-		// TODO Auto-generated method stub
-		if(this.endTime < o.endTime){
-			return -1;
-		}else if(this.endTime == o.endTime){
-			return Integer.compare(this.startTime, o.startTime);
+	public static void dfs(int depth, int index){
+		if(depth == m){
+			filedList.clear();
+			for(int i = 0; i < visitied.length; i++){
+				if(visitied[i]){filedList.add(arrayList.get(i));}
+			}
+			solvedChickenLength();
+		}else{
+			for(int i = index; i < arrayList.size(); i++){
+				if(visitied[i]){continue;}
+				else{
+					visitied[i] = true;
+					dfs(depth+1, i+1);
+					visitied[i] = false;
+				}
+			}
 		}
-		return 1;
+	}
+	
+	public static void solvedChickenLength(){
+		int totalLength = 0;
+		int length;
+		for (int i = 0; i < filed.length; i++) {
+			for (int j = 0; j < filed.length; j++) {
+				if(filed[i][j] == 1){
+					length = (int)Math.pow(filed.length, 2);
+					for (int j2 = 0; j2 < filedList.size(); j2++) {
+						length = Math.min(length, (Math.abs(i-filedList.get(j2)[0]) + Math.abs(j-filedList.get(j2)[1])));
+					}
+					totalLength += length;
+				}
+			}
+		}
+		
+		chickenlength = Math.min(chickenlength, totalLength);
 	}
 }
+
+
+
