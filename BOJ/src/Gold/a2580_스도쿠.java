733e88a8829c5294c4a75ca7a96f455a3c8232ff
younwony <wony9324@naver.com>
2020-01-09 17:17:22 +0900

    BOJ Gold(2580) ing

--- BOJ/src/Gold/a2580_스도쿠.java
+++ BOJ/src/Gold/a2580_스도쿠.java
@@ -28,39 +28,102 @@ public class a2580_스도쿠 {
 	 * @param args
 	 * 
 	 */
-	static Map<int[][], ArrayList<Integer>> map = new HashMap<int[][], ArrayList<Integer>>(); 
+	
+	static ArrayList<int[]> arrayList = new ArrayList<int[]>();
+	static int[][] sudokuFiled = new int[9][9];
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		int[][] sudokuFiled = new int[9][9];
 		for(int i = 0; i < 9; i++){
 			for(int j = 0; j < 9; j++){
 				sudokuFiled[i][j] = scanner.nextInt();
+				if(sudokuFiled[i][j] == 0){arrayList.add(new int[]{i,j});}
 			}
 		}
+		while(arrayList.size() != 0){
+			dfs();
+		}
 		scanner.close();
+		
+		for(int i = 0; i < 9; i++){
+			for(int j = 0; j < 9; j++){
+				System.out.print(sudokuFiled[i][j]+ " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void dfs(){
+		for(int i = 0; i < arrayList.size(); i++){
+			XYLineCheck(arrayList.get(i)[0],arrayList.get(i)[1]);
+			NineFiled(arrayList.get(i)[0], arrayList.get(i)[1]);
+			if(sudokuFiled[arrayList.get(i)[0]][arrayList.get(i)[1]] != 0){arrayList.remove(i);}
+		}
 	}
 	
 	/**
 	 * @작성자 wony
 	 * @작성일 2019. 12. 31.
-	 * @사용처
+	 * @사용처 X축, Y축 확인
 	 * @Todo
 	 * 
 	 */
-	public static void XYLineCheck(){
+	public static void XYLineCheck(int x, int y){
+		if(sudokuFiled[x][y] != 0) return;
+		boolean[] xChecked = new boolean[9];
+		boolean[] yChecked = new boolean[9];
 		
+		int xCount = 0;
+		int yCount = 0;
+		for(int i = 0; i < 9; i++){
+			if(i == y){continue;}
+			if(sudokuFiled[x][i] == 0){xCount++;continue;}
+			else{xChecked[sudokuFiled[x][i]-1] = true;}
+		}
+		for(int i = 0; i < 9; i++){
+			if(i == x){continue;}
+			if(sudokuFiled[i][y] == 0){yCount++;continue;}
+			else{yChecked[sudokuFiled[i][y]-1] = true;}
+		}
+		
+		if(xCount == 0){
+			for(int i = 0; i < 9; i++){
+				if(!xChecked[i]){sudokuFiled[x][y] = i+1; return ;}
+			}
+		}else if(yCount == 0){
+			for(int i = 0; i < 9; i++){
+				if(!yChecked[i]){sudokuFiled[x][y] = i+1; return ;}
+			}
+		}
 	}
 	
 	/**
 	 * @작성자 wony
 	 * @작성일 2019. 12. 31.
-	 * @사용처
+	 * @사용처 해당 점의 9칸확인
 	 * @Todo
 	 * 
 	 */
-	public static void NineFiled(){
+	public static void NineFiled(int x, int y){
+		if(sudokuFiled[x][y] != 0) return;
+		int xQuotient = x/3;
+		int yQuotient = y/3;
+		boolean[] checked = new boolean[9];
+		int xLocation;
+		int yLocation;
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				xLocation = (xQuotient*3)+i;
+				yLocation = (yQuotient*3)+j;
+				if(xLocation == x && yLocation == y){continue;}
+				if(sudokuFiled[xLocation][yLocation] == 0){return;}
+				else{checked[sudokuFiled[xLocation][yLocation]-1] = true;}
+			}
+		}
 		
+		for(int i = 0; i < 9; i++){
+			if(!checked[i]){sudokuFiled[x][y] = i+1; return;}
+		}
 	}
 
 }
--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -7,47 +7,87 @@ import java.util.*;
 
 public class Main {
 	
-	static int n;
-	static StringBuilder builder = new StringBuilder();
+	static ArrayList<int[]> arrayList = new ArrayList<int[]>();
+	static int[][] sudokuFiled = new int[9][9];
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		n = scanner.nextInt();
+		for(int i = 0; i < 9; i++){
+			for(int j = 0; j < 9; j++){
+				sudokuFiled[i][j] = scanner.nextInt();
+				if(sudokuFiled[i][j] == 0){arrayList.add(new int[]{i,j});}
+			}
+		}
+		while(arrayList.size() != 0){
+			dfs();
+		}
 		scanner.close();
-		dfs(0);
+		
+		for(int i = 0; i < 9; i++){
+			for(int j = 0; j < 9; j++){
+				System.out.print(sudokuFiled[i][j]+ " ");
+			}
+			System.out.println();
+		}
 	}
 	
-	public static void dfs(int depth){
-		if(depth == n){
-			System.out.println(builder.toString());
-			System.exit(0);
-		}else{
-			for(int i =1; i <= 3; i++){
-				builder.append(i);
-				if(isRight(builder.toString())){
-					dfs(depth+1);
-				}
-				builder.delete(builder.length()-1, builder.length());
-				
+	public static void dfs(){
+		for(int i = 0; i < arrayList.size(); i++){
+			XYLineCheck(arrayList.get(i)[0],arrayList.get(i)[1]);
+			NineFiled(arrayList.get(i)[0], arrayList.get(i)[1]);
+			if(sudokuFiled[arrayList.get(i)[0]][arrayList.get(i)[1]] != 0){arrayList.remove(i);}
+		}
+	}
+	
+	public static void XYLineCheck(int x, int y){
+		if(sudokuFiled[x][y] != 0) return;
+		boolean[] xChecked = new boolean[9];
+		boolean[] yChecked = new boolean[9];
+		
+		int xCount = 0;
+		int yCount = 0;
+		for(int i = 0; i < 9; i++){
+			if(i == y){continue;}
+			if(sudokuFiled[x][i] == 0){xCount++;continue;}
+			else{xChecked[sudokuFiled[x][i]-1] = true;}
+		}
+		for(int i = 0; i < 9; i++){
+			if(i == x){continue;}
+			if(sudokuFiled[i][y] == 0){yCount++;continue;}
+			else{yChecked[sudokuFiled[i][y]-1] = true;}
+		}
+		
+		if(xCount == 0){
+			for(int i = 0; i < 9; i++){
+				if(!xChecked[i]){sudokuFiled[x][y] = i+1; return ;}
+			}
+		}else if(yCount == 0){
+			for(int i = 0; i < 9; i++){
+				if(!yChecked[i]){sudokuFiled[x][y] = i+1; return ;}
 			}
 		}
 	}
 	
-	public static boolean isRight(String str){
-		
-		boolean result = true;
-		
-		for(int i = 1; i <= str.length()/2; i++){
-			for(int j = 0; j <= str.length()-2*i; j++){
-				if(str.substring(j,j+i).equals(str.substring(j+i, j+2*i))){
-					result = false;
-				}
+	public static void NineFiled(int x, int y){
+		if(sudokuFiled[x][y] != 0) return;
+		int xQuotient = x/3;
+		int yQuotient = y/3;
+		boolean[] checked = new boolean[9];
+		int xLocation;
+		int yLocation;
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				xLocation = (xQuotient*3)+i;
+				yLocation = (yQuotient*3)+j;
+				if(xLocation == x && yLocation == y){continue;}
+				if(sudokuFiled[xLocation][yLocation] == 0){return;}
+				else{checked[sudokuFiled[xLocation][yLocation]-1] = true;}
 			}
-			
 		}
 		
-		return result;
-		
+		for(int i = 0; i < 9; i++){
+			if(!checked[i]){sudokuFiled[x][y] = i+1; return;}
+		}
 	}
 }
 
