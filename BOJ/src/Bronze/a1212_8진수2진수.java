f652aec99b9f58d70de79edc25e60d92f69fcabc
younwony <wony9324@naver.com>
2020-01-14 17:43:57 +0900

    BOJ Bronze (1212) solved

--- BOJ/src/Bronze/a1212_8진수2진수.java
+++ BOJ/src/Bronze/a1212_8진수2진수.java
@@ -28,7 +28,22 @@ public class a1212_8진수2진수 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(Integer.toBinaryString(new Scanner(System.in).nextInt(8)));
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		String[] firstArray = {"0","1","10","11","100","101","110","111"};
+		String[] afterArray = {"000","001","010","011","100","101","110","111"};
+		
+		StringBuilder builder = new StringBuilder();
+		for(int i = 0; i < str.length(); i++){
+			if(i == 0){
+				builder.append(firstArray[str.charAt(i)-'0']);
+			}else{
+				builder.append(afterArray[str.charAt(i)-'0']);
+			}
+		}
+		System.out.println(builder.toString());
+		
+		scanner.close();
 	}
 
 }
--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -7,47 +7,24 @@ import java.util.*;
 
 public class Main {
 	
-	static int count = 0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		int m = scanner.nextInt();
-		scanner.close();
-		DP(n, m);
+		String str = scanner.nextLine();
+		String[] firstArray = {"0","1","10","11","100","101","110","111"};
+		String[] afterArray = {"000","001","010","011","100","101","110","111"};
 		
-		System.out.println(count);
-	}
-	
-	public static void DP(int n, int m){
-		if(n == 1 && m == 1){
-			return ;
-		}else if(n == 1){
-			count += m-1;
-			return ;
-		}else if(m == 1){
-			count += n-1;
-			return ;
-		}
-		if(n < m){
-			count++;
-			if(m%2 == 1){
-				DP(n, m/2);
-				DP(n, (m/2)+1);
+		StringBuilder builder = new StringBuilder();
+		for(int i = 0; i < str.length(); i++){
+			if(i == 0){
+				builder.append(firstArray[str.charAt(i)-'0']);
 			}else{
-				DP(n, m/2);
-				DP(n, m/2);
-			}
-		}else{
-			count++;
-			if(n%2 == 1){
-				DP(n/2, m);
-				DP(n/2+1, m);
-			}else{
-				DP(n/2, m);
-				DP(n/2, m);
+				builder.append(afterArray[str.charAt(i)-'0']);
 			}
 		}
+		System.out.println(builder.toString());
+		
+		scanner.close();
 	}
 }
 
