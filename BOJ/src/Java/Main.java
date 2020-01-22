74486dd7fd0b412b37b85134d857285b7c319420
younwony <wony9324@naver.com>
2020-01-22 20:10:40 +0900

    BOJ Bronze (2566) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -10,11 +10,26 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		int c = scanner.nextInt();
-		int d = scanner.nextInt();
-		System.out.println((int)Math.abs(b+c-a-d));
+		int max = 0;
+		int input;
+		int x = 0;
+		int y = 0;
+		
+		for(int i = 1; i <= 9; i++) {
+			for(int j = 1; j <= 9; j++) {
+				input = scanner.nextInt();
+				if(max < input) {
+					x = i;
+					y = j;
+					max = input;
+				}
+			}
+		}
+		
+		scanner.close();
+		
+		System.out.println(max);
+		System.out.println(x + " " + y);
 	}
 }
 
