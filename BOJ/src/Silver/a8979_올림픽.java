42ceebfadf79632668af1f5614a50ea7b39cb0e0
younwony <wony9324@naver.com>
2020-01-24 17:30:02 +0900

    BOJ Silver(8979) solved

--- BOJ/src/Silver/a8979_올림픽.java
+++ BOJ/src/Silver/a8979_올림픽.java
@@ -19,8 +19,8 @@ public class a8979_올림픽 {
 	/**
 	 * @작성자 wony
 	 * @작성일 2020. 1. 23.
-	 * @사용처
-	 * @Todo
+	 * @사용처 
+	 * @Todo 정렬로 풀어보다 실패함.. 비교문에서 실패한듯
 	 * @param args
 	 * 
 	 */
@@ -29,15 +29,43 @@ public class a8979_올림픽 {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
 		int k = scanner.nextInt();
-		int[][] inputData = new int[n][4];
-		int order;
+		
+		Olympic[] olympics = new Olympic[n+1];
 		for(int i = 0; i < n; i++){
-			order = scanner.nextInt();
-			for(int j = 0; j < 3; j++){
-				inputData[order][j] = scanner.nextInt();
-			}
+			int order = scanner.nextInt();
+			olympics[order] = new Olympic(scanner.nextInt(), scanner.nextInt(), scanner.nextInt());
 		}
+		
+		int count = 1;
+		
+		Olympic resultOl = olympics[k];
+		for(int i = 1; i <= n; i++){
+			Olympic isEqualsOl = olympics[i];
+			if (isEqualsOl.gold > resultOl.gold || (isEqualsOl.gold == resultOl.gold && isEqualsOl.silver > resultOl.silver) || (isEqualsOl.gold == resultOl.gold && isEqualsOl.silver == resultOl.silver && isEqualsOl.bronze > resultOl.bronze)) count++;
+		}
+		
+		System.out.println(count);
 	}
+	
+}
+
+class Olympic{
+	int order;
+	int gold;
+	int silver;
+	int bronze;
+	public Olympic() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Olympic(int gold, int silver, int bronze) {
+		super();
+		this.gold = gold;
+		this.silver = silver;
+		this.bronze = bronze;
+	}
+	
 
 }
 
