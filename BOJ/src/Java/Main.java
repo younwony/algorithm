c04a226efcaa44c1e2d9fb1dbf6ab80728b54c97
younwony <wony9324@naver.com>
2020-01-26 14:22:11 +0900

    BOJ Silver (10972) ~ing uu

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -7,21 +7,62 @@ import java.util.*;
 
 public class Main {
 	
+	static boolean[] visited;
+	static int[] resultArray;
+	static int[] inputArray;
+	static StringBuilder compareData = new StringBuilder();
+	static StringBuilder saveData = new StringBuilder(); 
+	static StringBuilder resultData = new StringBuilder();
+	static boolean checked = false;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		
-		int testCase = scanner.nextInt();
-		int[] inputData = new int[10];
-		
-		for(int i = 0; i < testCase; i++) {
-			for(int j = 0; j < 10; j++) {
-				inputData[j] = scanner.nextInt();
-			}
-			Arrays.sort(inputData);
-			System.out.println(inputData[7]);
+		int n = scanner.nextInt();
+		visited = new boolean[n];
+		resultArray = new int[n];
+		inputArray = new int[n];
+		for(int i = 0; i < n; i++) {
+			compareData.append(scanner.nextInt()+" ");
 		}
-		scanner.close();
+		dfs(n, 0);
+		
+		if("".equals(resultData.toString())) {
+			System.out.println(-1);
+		}else {
+			System.out.println(resultData.toString());
+		}
+	}
+	
+	public static void dfs(int n, int index) {
+		if(index == n) {
+			
+			saveData.setLength(0);
+			for(int i = 0; i < resultArray.length; i++) {
+				if(checked) {resultData.append(resultArray[i]+" ");}
+				else{saveData.append(resultArray[i]+" ");}
+			}
+			if(checked) {
+				if("".equals(resultData.toString())) {
+					System.out.println(-1);
+				}else {
+					System.out.println(resultData.toString());
+				}
+				System.exit(0);
+			}
+			if(compareData.toString().equals(saveData.toString())) {
+				checked = true;
+			}
+		}else {
+			for(int i = 0; i < n; i++) {
+				if(!visited[i]) {
+					visited[i] = true;
+					resultArray[index] = i + 1;
+					dfs(n, index+1);
+					visited[i] = false;
+				}
+			}
+		}
 	}
 }
 
