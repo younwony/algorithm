c563b9aee62d6b35b56d2bb0c1b601199608292f
younwony <wony9324@naver.com>
2020-01-29 12:05:07 +0900

    BOJ Silver (11726) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -7,29 +7,28 @@ import java.util.*;
 
 public class Main {
 	
+	static int[] dp;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		String n = scanner.nextLine();
+		
+		int n = scanner.nextInt();
+		
+		dp = new int[n+1];
+		dp[0] = 1;
+		dp[1] = 1;
+		
+		dp(n);
+		
+		System.out.println(dp[n]);
 		scanner.close();
-		double[] numArray = new double[10];
-		int inputData;
-		for(int i = 0; i<n.length(); i++) {
-			inputData = Character.getNumericValue(n.charAt(i));
-			if (inputData == 6 || inputData == 9) {
-				numArray[6] += 0.5;
-			} else {
-				numArray[inputData] += 1;
-			}
-
+	}
+	
+	public static void dp(int n) {
+		for(int i = 2; i <= n; i++) {
+			dp[i] = (dp[i-1] + dp[i-2])%10007;
 		}
-		
-		int max = 0;
-		for(double num : numArray) {
-			max = (int)Math.max(max, Math.round(num));
-		}
-		
-		System.out.println(max);
 	}
 }
 
