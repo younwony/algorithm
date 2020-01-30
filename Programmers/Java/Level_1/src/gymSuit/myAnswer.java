3383f00a63663db4e69f491f4dbf37c9593252f6
younwony <wony9324@naver.com>
2020-01-30 16:26:25 +0900

    Programmers Level_1 (mockText) solved

--- Programmers/Java/Level_1/src/gymSuit/myAnswer.java
+++ Programmers/Java/Level_1/src/gymSuit/myAnswer.java
@@ -29,6 +29,16 @@ public class myAnswer {
 		System.out.println(solution(5, new int[]{2,4}, new int[]{1,3,5}));
 		System.out.println(solution(5, new int[]{2,4}, new int[]{3}));
 		System.out.println(solution(3, new int[]{3}, new int[]{1}));
+		System.out.println(solution(4, new int[]{1,3}, new int[]{3}));
+		
+		System.out.println(solution(5, new int[]{2, 4}, new int[]{1, 3, 5}));
+		System.out.println(solution(5, new int[]{2, 4}, new int[]{3}));
+		System.out.println(solution(5, new int[]{1, 2}, new int[]{2, 3}));
+		System.out.println(solution(3, new int[]{1, 2}, new int[]{2, 3}));
+		System.out.println(solution(3, new int[]{1, 2, 3}, new int[]{1, 2, 3}));
+		System.out.println(solution(3, new int[]{3}, new int[]{1}));
+		System.out.println(solution(7, new int[]{2, 3, 4}, new int[]{1, 2, 3, 6}));
+		System.out.println(solution(5, new int[]{2, 3, 4}, new int[]{3, 4, 5}));
 	}
 	
 	/**
@@ -45,10 +55,35 @@ public class myAnswer {
 	public static int solution(int n, int[] lost, int[] reserve){
 		int answer = 0;
 		
-		answer = n - lost.length;
-
-		for(int i : lost){
-//			Arrays.
+		int[] total = new int[n];
+		
+		for(int i = 0; i < lost.length; i++){
+			total[lost[i]-1]--;
+		}
+		
+		for(int i = 0; i < reserve.length; i++){
+			total[reserve[i]-1]++;
+		}
+		
+		
+		for(int i = 0; i < total.length; i++){
+			if(total[i] == -1){
+				if(i > 0 && total[i-1] == 1){
+					total[i] = 0;
+					total[i-1] = 0;
+				}else if(i < total.length-1 && total[i+1] == 1){
+					total[i] = 0;
+					total[i+1] = 0;
+				}
+			}
+		}
+		
+		answer = n;
+		
+		for(int result : total){
+			if(result == -1){
+				answer--;
+			}
 		}
 		
 		return answer;
