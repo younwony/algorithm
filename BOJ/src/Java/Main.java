d8ed1e912d82ca254e52dbf8ca9da6d5e70c4eaa
younwony <wony9324@naver.com>
2020-01-31 13:38:35 +0900

    BOJ Test java create

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -7,37 +7,77 @@ import java.util.*;
 
 public class Main {
 	
+	static int zeroCount = 0;
+	static int[][] sudoku = new int[9][9];
+	static ArrayList<int[]> arrayList = new ArrayList<int[]>();
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		
-		int[] inputArray = new int[scanner.nextInt()];
-		
-		for(int i = 0; i < inputArray.length; i++){
-			inputArray[i] = scanner.nextInt();
+		for(int i = 0; i < 9; i++){
+			for(int j = 0; j < 9; j++){
+				sudoku[i][j] = scanner.nextInt();
+				if(sudoku[i][j] == 0){
+					arrayList.add(new int[]{i,j});
+					zeroCount++;
+				}
+			}
 		}
-		
 		scanner.close();
+		solvedBackTraking(0);
 		
-		System.out.println(dp(inputArray));
 	}
 	
-	public static int dp(int[] inputArray){
-		int[] dp = new int[inputArray.length];
-		dp[0] = inputArray[0];
-		for(int i = 1; i < inputArray.length; i++){
-			dp[i] = Math.max(inputArray[i], dp[i-1] + inputArray[i]);
+	public static void solvedBackTraking(int index){
+		if(zeroCount == index){
+			for(int i = 0; i < 9; i++){
+				for(int j = 0; j < 9; j++){
+					System.out.print(sudoku[i][j] + " ");
+				}
+				System.out.println();
+			}
+			System.exit(0);
+		}else{
+			int[] zeroFiled = arrayList.get(index);
+			for(int i = 1; i <10; i++){
+				sudoku[zeroFiled[0]][zeroFiled[1]] = i;
+				if(LineCheck(zeroFiled[0],zeroFiled[1]) && NineCheck(zeroFiled[0],zeroFiled[1])){
+					solvedBackTraking(index+1);
+				}
+				sudoku[zeroFiled[0]][zeroFiled[1]] = 0;
+			}
+		}
+	}
+	
+	public static boolean LineCheck(int x, int y){
+		
+		int checkNum = sudoku[x][y];
+		
+		for(int i = 0; i < 9; i++){
+			if((checkNum == sudoku[x][i] && i != y)|| (checkNum == sudoku[i][y] && i != x)){
+				return false;
+			}
 		}
 		
-		int max = dp[0];
+		return true;
+	}
+	
+	public static boolean NineCheck(int x, int y){
 		
-		for(int num : dp){
-			max = Math.max(num, max);
+		int checkNum = sudoku[x][y];
+		
+		int nX = x/3;
+		int nY = y/3;
+		
+		for(int i = nX*3; i < nX*3 + 3; i++){
+			for(int j = nY*3; j < nY*3 + 3; j++){
+				if(checkNum == sudoku[i][j] && i != x && j != y){
+					return false;
+				}
+			}
 		}
 		
-		return max;
-		
-		
+		return true;
 	}
 }
 
