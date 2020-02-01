a038f2f8564d9b4669d9cb9d7a5e3cfd7040c970
younwony <wony9324@naver.com>
2020-02-01 16:01:25 +0900

    BOJ Silver Level_5 (2485) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -10,18 +10,37 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		int sum = 0;
-		int n;
-		for(int i = 0; i < 10; i++){
-			n = scanner.nextInt();
-			if((int)Math.abs(sum-100) >= (int)Math.abs(sum+n-100)){
-				sum+=n;
-			}else{
-				break;
-			}
+		
+		int n = scanner.nextInt();
+		
+		int[] garosu = new int[n];
+		
+		int[] diffGarosu = new int[n-1];
+		
+		for(int i = 0; i < n; i++) {
+			garosu[i] = scanner.nextInt();
 		}
+		
 		scanner.close();
 		
-		System.out.println(sum);
+		for(int i = 0; i < n-1; i++) {
+			diffGarosu[i] = garosu[i+1] - garosu[i];
+		}
+		
+		int gcd = gcd(diffGarosu[0], diffGarosu[1]);
+		
+		for(int i = 1; i < n-2; i++) {
+			gcd = gcd(gcd, diffGarosu[i+1]);
+		}
+		
+		System.out.println(((garosu[n-1]-garosu[0])/gcd + 1) - n);
+	}
+	
+	public static int gcd(int a, int b) {
+		if(a == 0) {
+			return b;
+		}else {
+			return gcd(b%a, a);
+		}
 	}
 }
