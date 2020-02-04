1e4cdcc6367ae7c8b8d1a4595f53e7a1c7a6acea
younwony <wony9324@naver.com>
2020-02-04 10:53:38 +0900

    BOJ Gold Level_4 (15711) 소수판별법 수학적 내용으로 미진행

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -7,58 +7,46 @@ import java.util.*;
 
 public class Main {
 	
-	static boolean[] isDecimal = new boolean[1000001];
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
+		int testCase = scanner.nextInt();
 		
-		Eratos();
-		
-		int n;
-		
-		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		try {
-			while((n = scanner.nextInt()) != 0){
-				bufferedWriter.write(GoldBach(n) + "\n");
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}finally{
-			try {
-				if(bufferedWriter != null){bufferedWriter.flush(); bufferedWriter.close();}
-			} catch (Exception e2) {
-				// TODO: handle exception
-				e2.printStackTrace();
-			}
+		for(int i = 0; i < testCase; i++){
+			GoldBath(scanner.nextLong(),scanner.nextLong());
 		}
 		
 		scanner.close();
 	}
 	
-	public static void Eratos(){
+	public static void GoldBath(long a, long b){
 		
-		for (int i = 2; i < isDecimal.length; i++) {
-			isDecimal[i] = true;
+		boolean result = false;
+		
+		long sum = a+b;
+		
+		if(sum < 4){
+			result = true;
+		}else if(sum%2 == 0){
+			result = false;
+		}else {
+			result = isDecimal(sum-2);
 		}
 		
-		for(int i = 2; i*i < isDecimal.length; i++){
-			for(int j = i*i; j < isDecimal.length; j += i){
-				isDecimal[j] = false;
+		System.out.println(result ? "YES" : "NO");
+		
+	}		
+
+	public static boolean isDecimal(long num){
+		
+		for(long i = 2; i*i < num; i++){
+			for(long j = i; j < num; j+= i){
+				if(num % i == 0){
+					return false;
+				}
 			}
 		}
-	}
-	
-	public static String GoldBach(int n){
 		
-		for(int i = 2; i < isDecimal.length/2; i++){
-			if(isDecimal[i] && isDecimal[n-i]){
-				return  n + " = " + i + " + " + (n-i);
-			}
-		}
-		
-		return "Goldbach's conjecture is wrong."; 
-		
+		return true;
 	}
 }
