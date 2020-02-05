692d4110adce4fbbbb3fc85aa080514f3ae4da7f
younwony <wony9324@naver.com>
2020-02-05 20:34:13 +0900

    BOJ Bronze Level_4 (16204) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -10,43 +10,16 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		int testCase = scanner.nextInt();
 		
-		for(int i = 0; i < testCase; i++){
-			GoldBath(scanner.nextLong(),scanner.nextLong());
-		}
+		int n = scanner.nextInt();
+		int nO = scanner.nextInt();
+		int nX = n-nO;
+		int cO = scanner.nextInt();
+		int cX = n-cO;
 		
-		scanner.close();
-	}
-	
-	public static void GoldBath(long a, long b){
+		int o = nO > cO ? cO : nO;
+		int x = nX > cX ? cX : nX;
 		
-		boolean result = false;
-		
-		long sum = a+b;
-		
-		if(sum < 4){
-			result = true;
-		}else if(sum%2 == 0){
-			result = false;
-		}else {
-			result = isDecimal(sum-2);
-		}
-		
-		System.out.println(result ? "YES" : "NO");
-		
-	}		
-
-	public static boolean isDecimal(long num){
-		
-		for(long i = 2; i*i < num; i++){
-			for(long j = i; j < num; j+= i){
-				if(num % i == 0){
-					return false;
-				}
-			}
-		}
-		
-		return true;
+		System.out.println(o+x);
 	}
 }
