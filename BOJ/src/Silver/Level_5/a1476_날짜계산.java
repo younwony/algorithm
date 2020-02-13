6c41b10d51eebddf37b2c578185b14d39e77949f
younwony <wony9324@naver.com>
2020-02-13 19:56:40 +0900

    BOJ Silver Level_5 (1476) ing

---
+++ BOJ/src/Silver/Level_5/a1476_날짜계산.java
@@ -0,0 +1,40 @@
+/**
+ * @작성자 wony
+ * @작성일 2020. 2. 13.
+ * @사용처
+ * @Todo
+ */
+
+package Silver.Level_5;
+
+import java.util.Scanner;
+
+/**
+ * @작성자 wony
+ * @작성일 2020. 2. 13.
+ * @사용처
+ * @Todo
+ */
+
+public class a1476_날짜계산 {
+
+	/**
+	 * @작성자 wony
+	 * @작성일 2020. 2. 13.
+	 * @사용처 중국인의 나머지정리 공부
+	 * @param args
+	 * @Todo
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		int e = scanner.nextInt();
+		int s = scanner.nextInt();
+		int m = scanner.nextInt();
+		
+		scanner.close();
+	}
+
+}
