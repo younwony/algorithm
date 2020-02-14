df2a98a956683b56f65b76e5dc536c15fd66f0ad
younwony <wony9324@naver.com>
2020-02-14 19:37:09 +0900

    BOJ Bronze Level_2 (2309) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -6,32 +6,50 @@ import java.util.*;
 
 public class Main {
 	
+	public static int sum;
+	public static int[] nan = new int[9];
+	public static boolean[] visitied = new boolean[9];
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		
-		int testCase = scanner.nextInt();
-		
-		for(int i = 0; i < testCase; i++) {
-			Combination(scanner.nextInt(), scanner.nextInt());
+		for(int i = 0 ; i < 9; i++) {
+			 nan[i] = scanner.nextInt(); 
 		}
+		Arrays.sort(nan);
 		
-		scanner.close();
+		dfs(0, 0);
+		
+		
 	}
 	
-	public static void Combination(int n, int m) {
-		
-		long top = 1;
-		long bottom = 1;
-		for(int i = 0; i < n; i++) {
-			top *= m-i;
-			bottom *= i+1;
-			if(top % bottom == 0) {
-				top /= bottom;
-				bottom = 1;
+	public static void dfs(int index, int count) {
+		if(count == 7) {
+			sum = 0;
+			for(int i = 0 ; i< 9; i++) {
+				if(visitied[i]) {
+					sum+=nan[i];
+				}
+			}
+			if(sum == 100) {
+				for(int i = 0; i < 9; i++) {
+					if(visitied[i]) {
+						System.out.println(nan[i]);
+					}
+				}
+				System.exit(0);
+			}
+		}else {
+			for(int i = index ; i < 9; i ++) {
+				
+				visitied[i] = true;
+				count++;
+				dfs(i+1, count);
+				count--;
+				visitied[i] = false;
+				
 			}
 		}
-		
-		System.out.println(top/bottom);
 	}
 }
