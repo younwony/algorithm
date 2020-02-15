24ca004ada6301767560aae7172096506b0fddb1
younwony <wony9324@naver.com>
2020-02-15 17:29:36 +0900

    BOJ Bronze Level_2 (2743) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -6,50 +6,8 @@ import java.util.*;
 
 public class Main {
 	
-	public static int sum;
-	public static int[] nan = new int[9];
-	public static boolean[] visitied = new boolean[9];
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		
-		for(int i = 0 ; i < 9; i++) {
-			 nan[i] = scanner.nextInt(); 
-		}
-		Arrays.sort(nan);
-		
-		dfs(0, 0);
-		
-		
-	}
-	
-	public static void dfs(int index, int count) {
-		if(count == 7) {
-			sum = 0;
-			for(int i = 0 ; i< 9; i++) {
-				if(visitied[i]) {
-					sum+=nan[i];
-				}
-			}
-			if(sum == 100) {
-				for(int i = 0; i < 9; i++) {
-					if(visitied[i]) {
-						System.out.println(nan[i]);
-					}
-				}
-				System.exit(0);
-			}
-		}else {
-			for(int i = index ; i < 9; i ++) {
-				
-				visitied[i] = true;
-				count++;
-				dfs(i+1, count);
-				count--;
-				visitied[i] = false;
-				
-			}
-		}
+		System.out.println(new Scanner(System.in).nextLine().length());
 	}
 }
