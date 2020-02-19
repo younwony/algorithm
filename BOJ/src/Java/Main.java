dbc5519117b7dfc714c69d966433a9fac35bea9d
younwony <wony9324@naver.com>
2020-02-19 17:14:50 +0900

    BOJ Silver Level_4 (10773) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -8,63 +8,32 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+		Scanner scanner = new Scanner(System.in);
 		
-		try {
-			String nm = bufferedReader.readLine();
-			int n = nm.charAt(0) - '0';
-			int m = nm.charAt(2) - '0';
-			
-			String[] nData = bufferedReader.readLine().split(" ");
-			String[] mData = bufferedReader.readLine().split(" ");
-			
-			bufferedWriter.write(mergeSort(nData, mData));
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		} finally{
-			try {
-				if(bufferedReader != null){bufferedReader.close();}
-				if(bufferedWriter != null){bufferedWriter.flush(); bufferedWriter.close();}
-			} catch (Exception e2) {
-				// TODO: handle exception
-				e2.printStackTrace();
-			}
-		}
-	}
-	
-	public static String mergeSort(String[] n, String[] m){
-
-		int ni = 0;
-		int mi = 0;
-		int di = 0;
+		int testCase = scanner.nextInt();
 		
-		StringBuilder builder = new StringBuilder();
+		int index = -1;
 		
-		for(;di< n.length + m.length; di++){
-			if(Integer.parseInt(n[ni]) > Integer.parseInt(m[mi])){
-				builder.append(m[mi++] + " ");
+		int data;
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		
+		for(int i = 0; i < testCase; i++){
+			data = scanner.nextInt();
+			if(data == 0){
+				arrayList.remove(index--);
 			}else{
-				builder.append(n[ni++] + " ");
-			}
-			if(mi == m.length || ni == n.length){
-				di++;
-				break;
+				arrayList.add(data);
+				index++;
 			}
 		}
 		
-		if(ni < n.length){
-			for(; di < n.length + m.length; di++){
-				builder.append(n[ni++] + " ");
-			}
-		}else if(mi < m.length){
-			for(; di < n.length + m.length; di++){
-				builder.append(m[mi++] + " ");
-			}
+		scanner.close();
+		
+		int sum = 0;
+		for(int i : arrayList){
+			sum += i;
 		}
 		
-		return builder.toString();
+		System.out.println(sum);
 	}
 }
