6f90c141798cd11a2a3f5139279f98654911f3f2
younwony <wony9324@naver.com>
2020-02-20 10:12:50 +0900

    BOJ Silver Level_3 (2193) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -6,34 +6,29 @@ import java.util.*;
 
 public class Main {
 	
+	static long[][] zeroOne = new long[91][2];
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		
-		int testCase = scanner.nextInt();
-		
-		int index = -1;
-		
-		int data;
-		ArrayList<Integer> arrayList = new ArrayList<Integer>();
-		
-		for(int i = 0; i < testCase; i++){
-			data = scanner.nextInt();
-			if(data == 0){
-				arrayList.remove(index--);
-			}else{
-				arrayList.add(data);
-				index++;
-			}
-		}
-		
+		int n = scanner.nextInt();
 		scanner.close();
+
+		zeroOne[1][0] = 1;
+		zeroOne[1][1] = 0;
+		zeroOne[2][0] = 0;
+		zeroOne[2][1] = 1;
 		
-		int sum = 0;
-		for(int i : arrayList){
-			sum += i;
+		dp();
+		
+		System.out.println(zeroOne[n][0] + zeroOne[n][1]);
+		
+	}
+	
+	public static void dp(){
+		
+		for(int i = 3; i < 91; i++){
+			zeroOne[i][0] = zeroOne[i-1][0] + zeroOne[i-2][0];
+			zeroOne[i][1] = zeroOne[i-1][1] + zeroOne[i-2][1];
 		}
-		
-		System.out.println(sum);
 	}
 }
