f1993bf5e00b56d72df3227501a043e8ab6cea7d
younwony <wony9324@naver.com>
2020-02-22 15:54:55 +0900

    BOJ Silver Level_5 (4796) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -6,29 +6,21 @@ import java.util.*;
 
 public class Main {
 	
-	static long[][] zeroOne = new long[91][2];
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		scanner.close();
-
-		zeroOne[1][0] = 1;
-		zeroOne[1][1] = 0;
-		zeroOne[2][0] = 0;
-		zeroOne[2][1] = 1;
 		
-		dp();
+		int L,P,V;
 		
-		System.out.println(zeroOne[n][0] + zeroOne[n][1]);
+		int a,b;
 		
-	}
-	
-	public static void dp(){
-		
-		for(int i = 3; i < 91; i++){
-			zeroOne[i][0] = zeroOne[i-1][0] + zeroOne[i-2][0];
-			zeroOne[i][1] = zeroOne[i-1][1] + zeroOne[i-2][1];
+		int count = 1;
+		while((L=scanner.nextInt()) != 0 && (P=scanner.nextInt()) != 0 && (V=scanner.nextInt()) != 0){
+			a = V/P *L;
+			b = L > V%P ? V%P : L;
+			System.out.println("Case " + count++ + ": " + (a+b));
 		}
+		
+		scanner.close();
 	}
 }
