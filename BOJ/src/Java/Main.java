2c97ee4fc36bd824782cbc04a5101f8337d5d7c8
younwony <wony9324@naver.com>
2020-02-28 10:51:50 +0900

    BOJ Silver Level_4 (1940) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -9,41 +9,36 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
+		int testCase = scanner.nextInt();
+		int needNum = scanner.nextInt();
 		
-		int melonCount = scanner.nextInt();
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
 		
-		int[] size = new int[6];
-		
-		int x = 0,y = 0,xx = 0 ,yy = 0;
-		
-		for(int i = 0; i < 6; i++) {
-			scanner.nextInt();
-			size[i] = scanner.nextInt();
-			if(i % 2 == 0) {
-				if(size[i] > x) {
-					x = size[i];
-				}
-			}else {
-				if(size[i] > y) {
-					y = size[i];
-				}
-			}
+		for(int i = 0; i < testCase; i++) {
+			arrayList.add(scanner.nextInt());
 		}
 		
 		scanner.close();
 		
-		for(int i = 0; i < 6; i++) {
-			if(i % 2 == 0) {
-				if((size[(i+5)%6] + size[(i+1)%6]) == y) {
-					xx = size[i];
-				}
-			}else {
-				if((size[(i+5)%6] + size[(i+1)%6]) == x) {
-					yy = size[i];
-				}
+		int count = 0;
+		
+		arrayList.sort(null);
+		
+		int index;
+		for(int i = 0; i < testCase; i++) {
+			
+			if(arrayList.get(i) > needNum/2) {
+				break;
 			}
+			
+			index = arrayList.indexOf(needNum - arrayList.get(i));
+			if(index != -1 && index != i) {
+				count++;
+			}
+			
 		}
 		
-		System.out.println(melonCount * (x * y - xx* yy));
+		System.out.println(count);
+		
 	}
 }
