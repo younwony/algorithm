600e7a7546ab998e1ced94a80306c7521dfa7ac6
younwony <wony9324@naver.com>
2020-02-29 12:50:44 +0900

    BOJ Bronze Level_1 (1356) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -9,36 +9,38 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		int testCase = scanner.nextInt();
-		int needNum = scanner.nextInt();
 		
-		ArrayList<Integer> arrayList = new ArrayList<Integer>();
-		
-		for(int i = 0; i < testCase; i++) {
-			arrayList.add(scanner.nextInt());
-		}
+		String testNum = scanner.nextLine();
 		
 		scanner.close();
 		
-		int count = 0;
+		String n,m;
 		
-		arrayList.sort(null);
+		boolean isResult = false;
 		
-		int index;
-		for(int i = 0; i < testCase; i++) {
-			
-			if(arrayList.get(i) > needNum/2) {
+		for(int i = 0; i < testNum.length(); i++) {
+			n = testNum.substring(0,i);
+			m = testNum.substring(i, testNum.length());
+			if(isEqual(n, m)) {
+				isResult = true;
 				break;
 			}
-			
-			index = arrayList.indexOf(needNum - arrayList.get(i));
-			if(index != -1 && index != i) {
-				count++;
-			}
-			
 		}
 		
-		System.out.println(count);
+		System.out.println(isResult ? ("1".equals(testNum) ? "NO" :"YES") : "NO");
+	}
+	
+	public static boolean isEqual(String n, String m) {
+		int nCount = 1;
+		int mCount = 1;
 		
+		for(int i = 0; i < n.length(); i++) {
+			nCount *= n.charAt(i) - '0';
+		}
+		for(int i = 0; i < m.length(); i++) {
+			mCount *= m.charAt(i) - '0';
+		}
+		
+		return nCount == mCount;
 	}
 }
