8caf6e765f7f500e5ff2652642139b04ff97b6b3
younwony <wony9324@naver.com>
2020-03-01 18:13:24 +0900

    BOJ Bronze Level_1 (2033) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -10,37 +10,23 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		
-		String testNum = scanner.nextLine();
+		int number = scanner.nextInt();
 		
 		scanner.close();
 		
-		String n,m;
+		int mod = 10;
 		
-		boolean isResult = false;
-		
-		for(int i = 0; i < testNum.length(); i++) {
-			n = testNum.substring(0,i);
-			m = testNum.substring(i, testNum.length());
-			if(isEqual(n, m)) {
-				isResult = true;
-				break;
+		int modNum;
+		while(number/mod > 0) {
+			modNum = number%mod;
+			if(modNum >= mod/2) {
+				number += mod;
 			}
+			number -= modNum;
+			mod *= 10;
 		}
 		
-		System.out.println(isResult ? ("1".equals(testNum) ? "NO" :"YES") : "NO");
-	}
-	
-	public static boolean isEqual(String n, String m) {
-		int nCount = 1;
-		int mCount = 1;
+		System.out.println(number);
 		
-		for(int i = 0; i < n.length(); i++) {
-			nCount *= n.charAt(i) - '0';
-		}
-		for(int i = 0; i < m.length(); i++) {
-			mCount *= m.charAt(i) - '0';
-		}
-		
-		return nCount == mCount;
 	}
 }
