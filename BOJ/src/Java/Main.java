85e355cf2556e0e59bf253cd4a89bfdc4a6991c3
younwony <wony9324@naver.com>
2020-03-05 17:39:06 +0900

    BOJ Silver Level_4 (1764) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -8,35 +8,47 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int n = scanner.nextInt();
-		int k = scanner.nextInt();
-		
-		scanner.close();
-		
-		Queue<Integer> queue = new LinkedList<Integer>();
-		
-		for(int i = 1 ; i <= n; i++){
-			queue.offer(i);
-		}
-		
-		int i = 1;
-		
-		StringBuilder builder = new StringBuilder();
-		
-		builder.append("<");
-		while(!queue.isEmpty()){
-			if(i%k == 0){
-				builder.append(queue.poll()+", ");
-			}else{
-				queue.offer(queue.poll());
+		try {
+			String[] inputNM = bufferedReader.readLine().split(" ");
+			int n = Integer.valueOf(inputNM[0]);
+			int m = Integer.valueOf(inputNM[1]);
+			
+			Set<String> nDataArray = new HashSet<String>();
+			ArrayList<String> resultData = new ArrayList<String>();
+			
+			for(int i = 0; i < n; i++){
+				nDataArray.add(bufferedReader.readLine());
 			}
-			i++;
+			
+			String isData;
+			
+			for(int i = 0; i < m; i++){
+				isData = bufferedReader.readLine();
+				if(nDataArray.contains(isData)){
+					resultData.add(isData);
+				}
+			}
+			
+			resultData.sort(null);
+			bufferedWriter.write(resultData.size() + "\n");
+			for(String data : resultData){
+				bufferedWriter.write(data + "\n");
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally{
+			try {
+				if(bufferedReader != null){bufferedReader.close();}
+				if(bufferedWriter != null){bufferedWriter.flush();bufferedWriter.close();}
+			} catch (Exception e2) {
+				// TODO: handle exception
+				e2.printStackTrace();
+			}
 		}
-		builder.delete(builder.lastIndexOf(","), builder.length());
-		builder.append(">");
-		
-		System.out.println(builder.toString());
 	}
 }
