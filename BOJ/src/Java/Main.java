f497c666653eddc8626dfb4151752887196cb3ae
younwony <wony9324@naver.com>
2020-03-07 22:08:33 +0900

    BOJ Silver Level_5 (9656) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -8,32 +8,6 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		
-		String[] inputAB = scanner.nextLine().split(" ");
-		
-		scanner.close();
-		
-		String A = inputAB[0];
-		String B = inputAB[1];
-		
-		int minusLength = B.length() - A.length();
-		
-		int count;
-		
-		int minCount = B.length();
-		
-		for(int i = 0; i<= minusLength; i++){
-			count = 0;
-			for(int j = 0; j < A.length(); j++){
-				if(B.charAt(i+j) != A.charAt(j)){
-					count++;
-				}
-			}
-			
-			if(minCount >= count){minCount = count;}
-		}
-		
-		System.out.println(minCount);
+		System.out.println(new Scanner(System.in).nextInt()%2 == 0 ? "SK" : "CY");
 	}
 }
