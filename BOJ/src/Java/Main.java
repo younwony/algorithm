cfcea0a0c45cedff34cf128cdb053510969dae9d
younwony <wony9324@naver.com>
2020-03-18 20:55:20 +0900

    Mac Con

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -10,41 +10,9 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		
-		int n = scanner.nextInt();
-		int m = scanner.nextInt();
+		String a = scanner.nextLine();
+		String b = scanner.nextLine();
 		
-		int[] treeHightArray = new int[n];
-		
-		int maxHeight = 0;
-		
-		for(int i = 0; i < n; i++){
-			treeHightArray[i] = scanner.nextInt();
-			maxHeight = Math.max(maxHeight, treeHightArray[i]);
-		}
-
-		long sum;
-		int left = 0;
-		int right = maxHeight;
-		int middle = (left+right)/2;
-		
-		while(right >= left){
-			sum = 0;
-			for(int i = 0 ; i < n; i++){
-				sum = treeHightArray[i] - middle > 0 ? sum + treeHightArray[i] - middle : sum + 0;
-			}
-			if(sum >= m){
-				maxHeight = middle;
-				left = middle + 1;
-			}else{
-				right = middle - 1;
-			}
-			middle = (left+right)/2;
-			
-		}
-		
-		
-		scanner.close();
-		
-		System.out.println(maxHeight);
+		System.out.println(a.length() < b.length() ? "no" : "go");
 	}
 }
