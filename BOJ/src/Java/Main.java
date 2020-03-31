cf9aa4c0b72876ce990cb196d12bf0727d66495d
younwony <wony9324@naver.com>
2020-03-31 11:09:44 +0900

    BOJ Bronze Level_1 (2684) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -8,44 +8,41 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+		Scanner scanner = new Scanner(System.in);
 		
-		try {
-			
-			int testCase = Integer.valueOf(bufferedReader.readLine());
-			
-			String[] inputData = new String[2];
-			int max,testCount;
-			String maxIndex = "";
-			
-			StringBuilder result = new StringBuilder();
-			for(int i = 0 ; i < testCase; i++){
-				max = 0;
-				testCount = Integer.valueOf(bufferedReader.readLine());
-				for(int j = 0; j < testCount; j++){
-					inputData = bufferedReader.readLine().split(" ");
-					
-					if(Integer.valueOf(inputData[1]) > max){
-						max = Integer.valueOf(inputData[1]);
-						maxIndex = inputData[0];
-					}
-				}
-				result.append(maxIndex + "\n");
+		int n = scanner.nextInt();
+		
+		ArrayList<String> coinList = new ArrayList<>();
+		coinList.add("TTT");
+		coinList.add("TTH");
+		coinList.add("THT");
+		coinList.add("THH");
+		coinList.add("HTT");
+		coinList.add("HTH");
+		coinList.add("HHT");
+		coinList.add("HHH");
+		
+		scanner.nextLine();
+		
+		String inputData;
+		
+		String isCoin;
+		
+		int[] resultArray;
+		for(int i = 0 ;i < n; i++) {
+			inputData = scanner.nextLine();
+			resultArray = new int[8];
+			for(int j = 0 ; j<= 37; j++){
+				isCoin = inputData.substring(j, j+3);
+				resultArray[coinList.indexOf(isCoin)]++;
 			}
-			bufferedWriter.write(result.toString());
 			
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		} finally{
-			try {
-				if(bufferedReader != null){bufferedReader.close();}
-				if(bufferedWriter != null){bufferedWriter.flush(); bufferedWriter.close();}
-			} catch (Exception e2) {
-				// TODO: handle exception
-				e2.printStackTrace();
+			for(int k : resultArray){
+				System.out.print(k + " ");
 			}
+			System.out.println();
 		}
+		
+		scanner.close();
 	}
 }
