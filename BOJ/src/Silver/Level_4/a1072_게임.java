81cb3b0654c2713ce5d1232cd695541749bc19c2
younwony <wony9324@naver.com>
2020-04-20 16:49:19 +0900

    BOJ Silver Level_4(1072) ~ing

---
+++ BOJ/src/Silver/Level_4/a1072_게임.java
@@ -0,0 +1,16 @@
+package Silver.Level_4;
+
+import java.util.Scanner;
+
+public class a1072_게임 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		long x = scanner.nextLong();
+		long y = scanner.nextLong();
+		long z = (int)Math.floor(x/y);
+	}
+
+}
