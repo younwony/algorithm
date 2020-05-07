75cc554ca1d9035957bffc58445c06189954d844
younwony <wony9324@naver.com>
2020-05-07 17:53:59 +0900

    BOJ Silver Level_4 (1072) solved

--- BOJ/src/Silver/Level_4/a1072_게임.java
+++ BOJ/src/Silver/Level_4/a1072_게임.java
@@ -4,37 +4,34 @@ import java.util.Scanner;
 
 public class a1072_게임 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-		
-		float x = scanner.nextLong();
-		float y = scanner.nextLong();
-		float z = (float)(Math.floor(y/x * 100));
-		
-		int targetZ = 0;
-		int left = 0;
-		int right = 1000000001;
-		int result = -1;
-		int middle;
-		while(left <= right){
-			middle  = (left + right)/2;
-			
-			if(z == 99){
-				middle = -1;
-				break;
-			}
-			
-			targetZ = (int)(Math.floor((y+middle)/(x+middle)*100));
-			
-			if (z >= targetZ){
-				  result = middle + 1;
-				  left = middle + 1;
-		    }
-	        else{right = middle - 1;}
-		}
-		
-		System.out.println(result);
-	}
+	 public static void main(String[] args) {
+	      Scanner scanner = new Scanner(System.in);
+	      
+	      long X=scanner.nextLong();
+	      long Y=scanner.nextLong();
+	      long Z=100*Y/X;
+	      
+	      if(Z>=99) {
+	         System.out.println(-1);
+	      }else {
+	         
+	    	 long low=0;
+	         long mid=0;
+	         long high=1000000000;
+	         
+	         while(low<=high) {
+	            mid=(low+high)/2;
+	            if(Z<100*(Y+mid)/(X+mid)){
+	               high=mid-1;
+	            }else {
+	               low=mid+1;
+	            }
+	         }
+	         
+	         System.out.println(low);
+	      }
+	      
+	      scanner.close();
+	   }
 
 }
