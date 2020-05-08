d26df6ed18c53ec6a9309bca5643bde8d30d71a6
younwony <wony9324@naver.com>
2020-05-08 12:53:59 +0900

    BOJ Bronze Level_3 (2455) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -9,30 +9,16 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
+		int input,output;
+		int max = 0;
+		int count = 0;
 		
-		long x = scanner.nextLong();
-		long y = scanner.nextLong();
-		long z = (y*100)/x;
-		if(z == 99){
-			System.out.println(-1);
-			System.exit(0);
+		for(int i = 0; i < 4; i++){
+			output = scanner.nextInt();
+			input = scanner.nextInt();
+			count += input-output;
+			if(count > max){max = count;}
 		}
-		int left = 0;
-		int right = 1000000000;
-		int result = 0;
-		while(left <= right){
-			
-			int middle  = (left + right)/2;
-			
-			int targetZ = (int) ((100 * (x + middle)) / (y + middle));
-			
-			if (targetZ > z){
-				  right = middle - 1;
-				  result = middle;
-		    }
-	        else{left = middle + 1;}
-		}
-		
-		System.out.println(result);
+		System.out.println(max);
 	}
 }
