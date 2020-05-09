ae0240a1fd67f2f1b06408de73e047f757316a83
younwony <wony9324@naver.com>
2020-05-09 21:52:09 +0900

    BOJ Bronze Level_1 (1256) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -9,16 +9,34 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		int input,output;
-		int max = 0;
-		int count = 0;
 		
-		for(int i = 0; i < 4; i++){
-			output = scanner.nextInt();
-			input = scanner.nextInt();
-			count += input-output;
-			if(count > max){max = count;}
+		int n = scanner.nextInt();
+		
+		while(n>=4) {
+			
+			if(mod(n)) {
+				break;
+			}
+			n--;
 		}
-		System.out.println(max);
+		
+		System.out.println(n);
+	}
+	
+	public static boolean mod(int n) {
+		boolean result = false;
+		
+		while(n > 0) {
+			int mod = n%10;
+			if(mod == 4 || mod == 7) {
+				result = true;
+			}else {
+				result = false;
+				break;
+			}
+			n /= 10;
+		}
+		
+		return result;
 	}
 }
