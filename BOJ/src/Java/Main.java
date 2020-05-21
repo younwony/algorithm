3a7dcf1a2f420042bd3ac4dd7726dc9e2257a30e
younwony <wony9324@naver.com>
2020-05-21 16:27:43 +0900

    LeetCode CleanUp

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -1,23 +1,14 @@
 package Java;
 
-import java.io.*;
-import java.math.BigInteger;
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
 		
-		int L = scanner.nextInt();
-		int A = scanner.nextInt();
-		int B = scanner.nextInt();
-		int C = scanner.nextInt();
-		int D = scanner.nextInt();
-		
-		int aMod = (int)Math.ceil((double)A/C);
-		int bMod = (int)Math.ceil((double)B/D);
-		System.out.println(aMod > bMod ? L - aMod : L - bMod);
 	}
+	
 }
