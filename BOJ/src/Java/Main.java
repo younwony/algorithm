2c80e8cb7f108757b1aa3f00b4777af4542a0737
younwony <wony9324@naver.com>
2020-06-17 23:26:48 +0900

    BOJ Bronze Level_5(1271) Solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -1,33 +1,20 @@
 package Java;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		
-		int n = scanner.nextInt();
-		
-		int tempn = n;
-		
-		for(int i = 1; i <= 2*n - 1; i++){
-			if(i <= n){
-				for(int j = 0; j < i; j++){
-					System.out.print("*");
-				}
-			}else{
-				tempn--;
-				for(int j = 0; j < tempn; j++){
-					System.out.print("*");
-				}
-			}
-			System.out.println();
-		}
+		String[] mn = scanner.nextLine().split(" ");
+		BigInteger m = new BigInteger(mn[0]);
+		BigInteger n = new BigInteger(mn[1]);
+		System.out.println(m.divide(n));
+		System.out.println(m.mod(n));
 	}
 	
 }
