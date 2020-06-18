33b3833a19ec56527cc4cd92d8c92a10461aa7c4
younwony <wony9324@naver.com>
2020-06-18 23:31:02 +0900

    BOJ Bronze Level_4 (1297) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -10,11 +10,17 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String[] mn = scanner.nextLine().split(" ");
-		BigInteger m = new BigInteger(mn[0]);
-		BigInteger n = new BigInteger(mn[1]);
-		System.out.println(m.divide(n));
-		System.out.println(m.mod(n));
+
+		int n = scanner.nextInt();
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
+
+		double z = Math.sqrt(x*x + y*y);
+
+		double nRatio = n/z;
+
+		System.out.println((int)(x * nRatio));
+		System.out.println((int)(y * nRatio));
 	}
 	
 }
