f7bbca4ed91fed1385195ecc3389d947ed5392bb
younwony <wony9324@naver.com>
2020-06-21 20:22:34 +0900

    BOJ Bronze Level_3 (2446) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -10,17 +10,33 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
+		StringBuilder str = new StringBuilder();
 		int n = scanner.nextInt();
-		int x = scanner.nextInt();
-		int y = scanner.nextInt();
+		int m = 0;
+		int totalN = 2*n -1;
+		int totalm;
+		boolean isM = true;
+		for(int i = 0; i < totalN; i++){
+			totalm = 2*(n-m)-1;
+			for(int j = 0; j < m; j++){
+				str.append(" ");
+			}
+			for(int j = 0; j < totalm; j++){
+				str.append("*");
+			}
+			if(m == n-1){
+				isM = !isM;
+			}
+			if(isM){
+				m++;
+			}else{
+				m--;
+			}
+			str.append("\n");
+		}
 
-		double z = Math.sqrt(x*x + y*y);
-
-		double nRatio = n/z;
-
-		System.out.println((int)(x * nRatio));
-		System.out.println((int)(y * nRatio));
+		scanner.close();
+		System.out.println(str.toString());
 	}
-	
 }
+
