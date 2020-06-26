21f106becd70349dae8e48674f696c609069f193
younwony <wony9324@naver.com>
2020-06-26 09:44:00 +0900

    BOJ Silver Level_4 (1205) solved

--- BOJ/src/Silver/Level_4/a1205_등수구하기.java
+++ BOJ/src/Silver/Level_4/a1205_등수구하기.java
@@ -12,21 +12,27 @@ public class a1205_등수구하기 {
 		int score = scanner.nextInt();
 		int p = scanner.nextInt();
 		
-		int[] nArray = new int[n];
-		
-		int count = 1;
-		
-		for(int i = 0 ; i < n; i++){
-			nArray[i] = scanner.nextInt();
-			if(nArray[i] > score){
-				count++;
+		if(n > 0){
+			int[] nArray = new int[n];
+			
+			int count = 1;
+			
+			for(int i = 0 ; i < n; i++){
+				nArray[i] = scanner.nextInt();
+				if(nArray[i] > score){
+					count++;
+				}
 			}
-		}
-		
-		if(nArray[n-1] >= score && n == p){
-			System.out.println(-1);
+			
+			
+			
+			if(nArray[n-1] >= score && n == p){
+				System.out.println(-1);
+			}else{
+				System.out.println(count);
+			}
 		}else{
-			System.out.println(count);
+			System.out.println(1);
 		}
 		
 		scanner.close();
