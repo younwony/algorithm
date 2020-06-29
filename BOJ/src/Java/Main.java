49610ca596e203534bca08ab9099182024a3b56a
younwony <wony9324@naver.com>
2020-06-29 21:15:57 +0900

    BOJ Silver Level_5 (2563) solved

--- BOJ/src/Java/Main.java
+++ BOJ/src/Java/Main.java
@@ -6,27 +6,40 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 
-		int e = scanner.nextInt();
-		int s = scanner.nextInt();
-		int m = scanner.nextInt();
+		int n = scanner.nextInt();
+
+		int[][] field = new int[101][101];
+
+		int x,y;
+
+		for(int i = 0; i < n; i++){
+			x = scanner.nextInt();
+			y = scanner.nextInt();
+			for(int j = x; j < x +10; j++){
+				if(j > 100){
+					break;
+				}
+				for(int k = y; k < y + 10; k++){
+					if(k > 100){
+						break;
+					}
+					field[j][k] = 1;
+				}
+			}
+		}
+		scanner.close();
 
 		int result = 0;
 
-		int eNum,sNum,mNum;
-		while(true){
-			result++;
-			eNum = result % 15 == 0 ? 15 : result % 15;
-			sNum = result % 28 == 0 ? 28 : result % 28;
-			mNum = result % 19 == 0 ? 19 : result % 19;
-			if(eNum == e && sNum == s && mNum == m){
-				break;
+		for(int i = 0; i < field.length; i++){
+			for(int j = 0; j < field[0].length; j++){
+				result += field[i][j];
 			}
 		}
+
 		System.out.println(result);
-		scanner.close();
 	}
 		
 }
