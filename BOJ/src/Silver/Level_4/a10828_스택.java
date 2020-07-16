bf0b0fcd3c3fde8c1f36ab877588ace7ad61a76e
younwony <wony9324@naver.com>
2020-07-16 21:17:21 +0900

    BOJ Silver Level_4 (10828) Stack Study Again

--- BOJ/src/Silver/Level_4/a10828_스택.java
+++ BOJ/src/Silver/Level_4/a10828_스택.java
@@ -117,4 +117,47 @@ public class a10828_스택 {
 		else
 			return stack[0];
 	}
+
+	/**
+	 * Stack Lib 좀더 빠르게
+	 *
+	 *
+	 * static Stack<Integer> stack = new Stack<>();
+	 *     static StringBuilder result = new StringBuilder();
+	 *     static String[] inputData;
+	 *     public static void main(String[] args) {
+	 *         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	 *
+	 *         try {
+	 *             int n = Integer.valueOf(bufferedReader.readLine());
+	 *             for(int i = 0 ; i < n; i++){
+	 *                 inputData = bufferedReader.readLine().split(" ");
+	 *                 stackOrder(inputData[0]);
+	 *             }
+	 *             System.out.println(result.toString());
+	 *         }catch (Exception e){
+	 *             e.printStackTrace();
+	 *         }finally {
+	 *             try {
+	 *                 if(bufferedReader != null){bufferedReader.close();}
+	 *             }catch (Exception e2){
+	 *                 e2.printStackTrace();
+	 *             }
+	 *         }
+	 *     }
+	 *
+	 *     public static void stackOrder(String order){
+	 *         if("push".equals(order)){
+	 *             stack.push(Integer.valueOf(inputData[1]));
+	 *         }else if("pop".equals(order)){
+	 *             result.append((stack.empty() ? "-1" : stack.pop()) + "\n");
+	 *         }else if("size".equals(order)){
+	 *             result.append(Integer.toString(stack.size()) + "\n");
+	 *         }else if("empty".equals(order)){
+	 *             result.append((stack.empty() ? "1" : "0") + "\n");
+	 *         }else if("top".equals(order)){
+	 *             result.append((stack.empty() ? "-1" : stack.peek()) + "\n");
+	 *         }
+	 *     }
+	 */
 }
