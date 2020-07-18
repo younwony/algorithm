dc10c88e949e4ded4fe2e477509719aa49ee45e4
younwony <wony9324@naver.com>
2020-07-18 13:17:38 +0900

    BOJ Silver Level_4 (10866) Deque Again Study .Out ADd

--- BOJ/src/Silver/Level_4/a10866_덱.java
+++ BOJ/src/Silver/Level_4/a10866_덱.java
@@ -145,4 +145,52 @@ public class a10866_덱 {
 		return data;
 	}
 
+	/**
+	 *
+	 * 좀더빠른 라이브러리 덱
+	 *
+	 *  static Deque<Integer> deque = new ArrayDeque<>();
+	 *     static StringBuilder result = new StringBuilder();
+	 *     static String[] inputData;
+	 *     public static void main(String[] args) {
+	 *         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	 *
+	 *         try {
+	 *             int n = Integer.valueOf(bufferedReader.readLine());
+	 *             for(int i = 0 ; i < n; i++){
+	 *                 inputData = bufferedReader.readLine().split(" ");
+	 *                 stackOrder(inputData[0]);
+	 *             }
+	 *             System.out.println(result.toString());
+	 *         }catch (Exception e){
+	 *             e.printStackTrace();
+	 *         }finally {
+	 *             try {
+	 *                 if(bufferedReader != null){bufferedReader.close();}
+	 *             }catch (Exception e2){
+	 *                 e2.printStackTrace();
+	 *             }
+	 *         }
+	 *     }
+	 *
+	 *     public static void stackOrder(String order){
+	 *         if("push_front".equals(order)){
+	 *             deque.offerFirst(Integer.valueOf(inputData[1]));
+	 *         }else if("push_back".equals(order)){
+	 *             deque.offerLast(Integer.valueOf(inputData[1]));
+	 *         }else if("pop_front".equals(order)){
+	 *             result.append((deque.isEmpty() ? "-1" : deque.pollFirst()) + "\n");
+	 *         }else if("pop_back".equals(order)){
+	 *             result.append((deque.isEmpty() ? "-1" : deque.pollLast()) + "\n");
+	 *         }else if("size".equals(order)){
+	 *             result.append(Integer.toString(deque.size()) + "\n");
+	 *         }else if("empty".equals(order)){
+	 *             result.append((deque.isEmpty() ? "1" : "0") + "\n");
+	 *         }else if("front".equals(order)){
+	 *             result.append((deque.isEmpty() ? "-1" : deque.peekFirst()) + "\n");
+	 *         }else if("back".equals(order)){
+	 *             result.append((deque.isEmpty() ? "-1" : deque.peekLast()) + "\n");
+	 *         }
+	 *     }
+	 */
 }
