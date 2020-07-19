b91ccb3a597c6eb2a177f968378acaad79731844
younwony <wony9324@naver.com>
2020-07-19 18:17:12 +0900

    BOJ Silver Level_3 (1406) LinkedList Again Study

--- BOJ/src/Silver/Level_3/a1406_에디터.java
+++ BOJ/src/Silver/Level_3/a1406_에디터.java
@@ -118,4 +118,64 @@ public class a1406_에디터 {
 		return point + 1;
 	}
 
+	/**
+	 * Deque 2개 로 처리
+	 *
+	 * static Deque<String> deque = new ArrayDeque<>();
+	 *     static Deque<String> dequeClone = new ArrayDeque<>();
+	 *     static StringBuilder result = new StringBuilder();
+	 *     static String[] inputData;
+	 *     public static void main(String[] args) {
+	 *         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	 *
+	 *         try {
+	 *             String inputDataString = bufferedReader.readLine();
+	 *             for(int i = 0 ; i < inputDataString.length(); i++){
+	 *                 deque.offer(String.valueOf(inputDataString.charAt(i)));
+	 *             }
+	 *
+	 *             int n = Integer.valueOf(bufferedReader.readLine());
+	 *             for(int i = 0 ; i < n; i++){
+	 *                 inputData = bufferedReader.readLine().split(" ");
+	 *                 Order(inputData[0]);
+	 *             }
+	 *
+	 *             while(!deque.isEmpty()){
+	 *                 result.append(deque.pollFirst());
+	 *             }
+	 *
+	 *             while(!dequeClone.isEmpty()){
+	 *                 result.append(dequeClone.pollFirst());
+	 *             }
+	 *             System.out.println(result.toString());
+	 *         }catch (Exception e){
+	 *             e.printStackTrace();
+	 *         }finally {
+	 *             try {
+	 *                 if(bufferedReader != null){bufferedReader.close();}
+	 *             }catch (Exception e2){
+	 *                 e2.printStackTrace();
+	 *             }
+	 *         }
+	 *     }
+	 *
+	 *     public static void Order(String order){
+	 *         if("L".equals(inputData[0])){
+	 *             if(!deque.isEmpty()){
+	 *                 dequeClone.offerFirst(deque.pollLast());
+	 *             }
+	 *         }else if("D".equals(inputData[0])){
+	 *             if(!dequeClone.isEmpty()){
+	 *                 deque.offer(dequeClone.pollFirst());
+	 *             }
+	 *         }else if("B".equals(inputData[0])){
+	 *             if(!deque.isEmpty()){
+	 *                 deque.pollLast();
+	 *             }
+	 *         }else if("P".equals(inputData[0])){
+	 *             deque.offer(inputData[1]);
+	 *         }
+	 *
+	 *     }
+	 */
 }
