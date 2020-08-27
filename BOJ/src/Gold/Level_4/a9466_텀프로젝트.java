a870c6d8512ffddac9296ef6eace219362513335
younwony <wony9324@naver.com>
2020-08-27 15:28:53 +0900

    BOj Gold Level_4 (9466) solved

--- BOJ/src/Gold/Level_4/a9466_텀프로젝트.java
+++ BOJ/src/Gold/Level_4/a9466_텀프로젝트.java
@@ -1,16 +1,15 @@
 package Gold.Level_4;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class a9466_팀프로젝트 {
+public class a9466_텀프로젝트 {
 
 	static int count = 0;
 	static int[] field;
 	static boolean[] visited;
-	static boolean[] finished;
+	static boolean[] cycle;
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 		
@@ -19,6 +18,7 @@ public class a9466_팀프로젝트 {
 		int testCase = Integer.valueOf(bufferedReader.readLine());
 		StringBuilder result = new StringBuilder();
 		for(int i = 0; i < testCase; i++){
+			count = 0;
 			int personCnt = Integer.valueOf(bufferedReader.readLine());
 			field = new int[personCnt+1];
 			StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
@@ -26,14 +26,13 @@ public class a9466_팀프로젝트 {
 				field[x] = Integer.valueOf(stringTokenizer.nextToken());
 			}
 			
-			int resultCnt =  0;
 			visited = new boolean[personCnt+1];
-			finished = new boolean[personCnt+1];
+			cycle = new boolean[personCnt+1];
 			for(int x = 1; x <= personCnt; x++){
-				if(!visited[x])	dfs(x);
+				if(!visited[x] && !cycle[x])dfs(x);
 			}
 			
-			result.append(resultCnt).append("\n");
+			result.append(personCnt - count).append("\n");
 			
 		}
 		
@@ -44,20 +43,22 @@ public class a9466_팀프로젝트 {
 	public static void dfs(int x){
 		
 		visited[x] = true;
+		
 		int nextX = field[x];
 		
 		if(!visited[nextX]){
 			dfs(nextX);
 		}else{
-			if(!finished[nextX]){
-				for(int i = nextX; i != x; i = field[i]){
+			if(!cycle[nextX]){
+				while(x != nextX){
+					nextX = field[nextX];
 					count++;
 				}
 				count++;
 			}
 		}
 		
-		finished[x] = true;
+		cycle[x] = true;
 		
 	}
 
