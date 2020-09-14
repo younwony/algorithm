d649c6f8c82f09a73eec4c6b48abfb0a583ed19c
younwony <wony9324@naver.com>
2020-09-14 17:21:50 +0900

    BOJ Silver Level_1(11053) ~ing

---
+++ BOJ/src/Silver/Level_1/a11053_가장긴증가하는부분수열.java
@@ -0,0 +1,24 @@
+package Silver.Level_1;
+
+import java.util.Scanner;
+
+public class a11053_가장긴증가하는부분수열 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
+		int[] nArray = new int[n];
+		int[] dp = new int[n];
+		for(int i = 0 ; i < n; i++){
+			nArray[i] = scanner.nextInt();
+		}
+		
+		int max = nArray[0];
+		for(int i = 0 ; i < n; i++){
+			
+		}
+	}
+
+}
