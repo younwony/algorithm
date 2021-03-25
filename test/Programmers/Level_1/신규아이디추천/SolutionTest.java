e7037b4131e10d03f8a9688a323f1b95732e0bb4
younwony <dnjsl22@>
2021-03-25 17:44:35 +0900

    1. 프로그래머스 Level 1 - 신규 아이디 추천 완료

--- test/Programmers/Level_1/신규아이디추천/SolutionTest.java
+++ test/Programmers/Level_1/신규아이디추천/SolutionTest.java
@@ -12,53 +12,68 @@ class SolutionTest {
     void test(){
         //String new_id = "...!@BaT#*..y.abcdefghijklm";
         //String new_id = "z-+.^.";
-        //String new_id = "=.=";
+        String new_id = "=.=";
         //String new_id =	"123_.def";
         //String new_id = "abcdefghijklmn.p";
-        String new_id = "abcdefghijklmn.";
+        //String new_id = "abcdefghijklmn.";
 
         String answer = getString(new_id);
-        while(answer != getString(answer)){
-            answer = getString(answer);
-        }
         System.out.println(answer);
     }
 
     private String getString(String new_id) {
-        //1
-        String s = new_id.toLowerCase();
-        //2
-        String s2 = s.replaceAll("[^0-9a-z.\\-_]", "");
-        //3
-        while(s2.indexOf("..") > -1){
-            s2 = s2.replace("..", ".");
-        }
-        String s3 = s2;
-        //4
-        if(s3.charAt(0) == '.') {
-            s3 = s3.substring(1);
-        }
-        if(s3.length() > 0 && s3.charAt(s3.length()-1) == '.') {
-            s3 = s3.substring(0, s3.length()-1);
-        }
-        //5
-        String s4 = s3;
-        if("".equals(s3)) s4 = "a";
+        String answer = "";
 
-        //6
-        String s5 = s4;
-        if(s4.length() >= 16){
-            s5 = s4.substring(0, 15);
+        /*
+         * 1. 대문자 -> 소문자 치환
+         * */
+        answer = new_id.toLowerCase();
+
+        /*
+         * 2. 소문자, 숫자, -, _, . 를 제외한 문자 제거
+         * */
+        answer = answer.replaceAll("[^0-9a-z.\\-_]", "");
+
+        /*
+         * 3. .문자 2번 연속 -> . 하나로 치환
+         * */
+        while(answer.indexOf("..") > -1){
+            answer = answer.replace("..", ".");
         }
 
-        //7
-        String s6 = s5;
-        while(s6.length() <= 2){
-            char c = s6.charAt(s6.length()-1);
-            s6 += c;
+        /*
+         * 4. .문자 처음이나 끝에 위치할 경우 제거
+         * */
+        if(answer.charAt(0) == '.') {
+            answer = answer.substring(1);
+        }
+        if(answer.length() > 0 && answer.charAt(answer.length()-1) == '.') {
+            answer = answer.substring(0, answer.length()-1);
         }
 
-        String answer = s6;
+        /*
+         * 5. 빈 문자열 -> a 대입
+         * */
+        if("".equals(answer)) answer = "a";
+
+        /*
+         * 6. 문자열이 16자 이상이면 15문자를 제외한 나머지 문자 제거, 제거 후 . 문자가 마지막 문자면 .문자 제거
+         * */
+        if(answer.length() >= 16){
+            answer = answer.substring(0, 15);
+        }
+        if(answer.length() > 0 && answer.charAt(answer.length()-1) == '.') {
+            answer = answer.substring(0, answer.length()-1);
+        }
+
+        /*
+         * 7. 문자열이 2자 이하라면, 문자열의 마지막 문자를 길이가 3자 될때까지 반복
+         * */
+        while(answer.length() <= 2){
+            char c = answer.charAt(answer.length()-1);
+            answer += c;
+        }
+
         return answer;
     }
 }
