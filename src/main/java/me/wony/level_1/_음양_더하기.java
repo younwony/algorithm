11576b080996ad27bb9c78047f2309062ffe0554
younwony <wony9324@naver.com>
2021-04-25 21:13:51 +0900

    [수정] 프로그래머스 - lv1 - 음양 더하기 코드 정리

--- src/main/java/me/wony/level_1/_음양_더하기.java
+++ src/main/java/me/wony/level_1/_음양_더하기.java
@@ -1,4 +1,6 @@
-class Solution {
+package me.wony.level_1;
+
+public class _음양_더하기 {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
         for(int i = 0; i < absolutes.length; i++){
