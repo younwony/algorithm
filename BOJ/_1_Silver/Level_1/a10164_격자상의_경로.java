e0354c655e3b8417e036466c68add592251562f8
younwony <wony9324@naver.com>
2021-07-15 11:22:44 +0900

    [추가] BOJ - Silver 1 - 격자상의 경로 풀이 완료

--- BOJ/_1_Silver/Level_1/a10164_격자상의_경로.java
+++ BOJ/_1_Silver/Level_1/a10164_격자상의_경로.java
@@ -3,8 +3,6 @@ package _1_Silver.Level_1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class a10164_격자상의_경로 {
@@ -19,49 +17,28 @@ public class a10164_격자상의_경로 {
         int M = Integer.parseInt(stringTokenizer.nextToken());
         int K = Integer.parseInt(stringTokenizer.nextToken());
 
-        int kX = (K-1)/M;
-        int kY = (K-1)%M;
+        int[][] dp = new int[N+1][M+1];
+        dp[0][1] = 1;
 
-        Queue<Point> queue = new LinkedList<>();
-
-        queue.add(new Point(0,0,K == 0 ? true : false));
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[i].length; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
 
         int cnt = 0;
+        if(K > 0){
+            int kX = (K-1)/M + 1;
+            int kY = (K-1)%M + 1;
+            cnt = dp[kX][kY] * dp[N - kX + 1][M - kY + 1];
+        }
 
-        while(!queue.isEmpty()){
-            Point point = queue.poll();
-            if(point.x == N - 1 && point.y == M - 1){
-                if(point.isK) cnt++;
-                continue;
-            }
-
-            for(int i = 0 ; i < 2; i++){
-                int x = point.x + xArray[i];
-                int y = point.y + yArray[i];
-                boolean isK = point.isK;
-                if(x < 0 || y < 0 || x >= N || y >= M){
-                    continue;
-                }
-                if(!isK){
-                    isK = x == kX && y == kY;
-                }
-
-                queue.add(new Point(x,y,isK));
-            }
+        if(K == 0){
+            cnt = dp[N][M];
         }
 
         System.out.println(cnt);
+
     }
 
-    static class Point{
-        int x;
-        int y;
-        boolean isK;
-
-        public Point(int x, int y, boolean isK) {
-            this.x = x;
-            this.y = y;
-            this.isK = isK;
-        }
-    }
 }
