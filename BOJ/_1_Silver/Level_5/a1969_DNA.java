a094a5332c4506aa4d07ae4e1161e4bf4fd5a445
younwony <wony9324@naver.com>
2021-11-26 08:20:56 +0900

    [추가] BOJ - Silver 5 - DNA 풀이 완료

--- BOJ/_1_Silver/Level_5/a1969_DNA.java
+++ BOJ/_1_Silver/Level_5/a1969_DNA.java
@@ -9,32 +9,40 @@ import java.util.StringTokenizer;
 
 public class a1969_DNA {
     public static void main(String[] args) throws IOException {
+
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
         int n = Integer.parseInt(stringTokenizer.nextToken());
         int m = Integer.parseInt(stringTokenizer.nextToken());
-
-        HashMap<Character, Integer> dnaCtoI = new HashMap<>();
-        HashMap<Integer, Character> dnaItoC = new HashMap<>();
-        dnaCtoI.put('A', 0);
-        dnaCtoI.put('T', 1);
-        dnaCtoI.put('G', 2);
-        dnaCtoI.put('C', 3);
-        dnaItoC.put(0, 'A');
-        dnaItoC.put(1, 'T');
-        dnaItoC.put(2, 'G');
-        dnaItoC.put(3, 'C');
-
-        int[][] dnas = new int[m][4];
+        String[] dnas = new String[n];
+        int[][] check = new int[m]['Z' - 'A' + 1];
         for(int i = 0; i < n; i++){
-            String dna = bufferedReader.readLine();
-            for(int j = 9; j < m; j++){
-                char c = dna.charAt(j);
-                dnas[j][dnaCtoI.get(c)]++;
+            dnas[i] = bufferedReader.readLine();
+            for(int j = 0; j < m; j++){
+                int cInt = dnas[i].charAt(j) - 'A';
+                check[j][cInt]++;
             }
         }
+
+        StringBuilder dna =  new StringBuilder();
+        int sum = 0;
+        for(int i = 0; i < m; i++){
+            int maxCharIndex = 0;
+            for(int j = 'A'; j <= 'Z'; j++){
+                if(check[i][j - 'A'] > check[i][maxCharIndex]){
+                    maxCharIndex = j - 'A';
+                }
+            }
+            sum += n - check[i][maxCharIndex];
+            dna.append((char)(maxCharIndex + 'A'));
+        }
+        dna
+                .append("\n")
+                .append(sum);
+
+        System.out.println(dna.toString());
 
     }
 }
