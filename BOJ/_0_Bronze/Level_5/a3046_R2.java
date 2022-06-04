4b0a35b2363a5257d907c652b40fb5c456404af7
younwony <wony9324@naver.com>
2022-06-05 03:43:05 +0900

    feat: BOJ - Bronze 5 - 3046. R2

---
+++ BOJ/_0_Bronze/Level_5/a3046_R2.java
@@ -0,0 +1,12 @@
+package _0_Bronze.Level_5;
+
+import java.util.Scanner;
+
+public class a3046_R2 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        int R1 = scanner.nextInt();
+        int S = scanner.nextInt();
+        System.out.println(2 * S - R1);
+    }
+}
