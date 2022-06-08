28ec6e3f05c94ad3a75a7796c2062e520e41aba7
younwony <wony9324@naver.com>
2022-06-08 13:26:18 +0900

    feat: BOJ - Bronze 5 - 10699. 오늘 날짜

---
+++ BOJ/_1_Silver/Level_5/a10699_오늘_날짜.java
@@ -0,0 +1,9 @@
+package _1_Silver.Level_5;
+
+import java.time.LocalDate;
+
+public class a10699_오늘_날짜 {
+    public static void main(String[] args) {
+        System.out.println(LocalDate.now());
+    }
+}
