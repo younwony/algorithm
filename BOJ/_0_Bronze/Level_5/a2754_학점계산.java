0e0aaeebbdce83630981cfa30dbde05260e4d572
younwony <wony9324@naver.com>
2022-07-17 21:17:52 +0900

    feat: BOJ - Bronze 5 - 2754. 학점계산

---
+++ BOJ/_0_Bronze/Level_5/a2754_학점계산.java
@@ -0,0 +1,30 @@
+package _0_Bronze.Level_5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class a2754_학점계산 {
+    public static void main(String[] args) throws IOException {
+        HashMap<String, String> map = new HashMap<>();
+
+        map.put("A+", "4.3");
+        map.put("A0", "4.0");
+        map.put("A-", "3.7");
+        map.put("B+", "3.3");
+        map.put("B0", "3.0");
+        map.put("B-", "2.7");
+        map.put("C+", "2.3");
+        map.put("C0", "2.0");
+        map.put("C-", "1.7");
+        map.put("D+", "1.3");
+        map.put("D0", "1.0");
+        map.put("D-", "0.7");
+        map.put("F", "0.0");
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(map.get(bufferedReader.readLine()));
+    }
+}
