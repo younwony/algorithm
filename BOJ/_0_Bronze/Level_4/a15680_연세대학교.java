6ed671f3b7f99ed53c311f2d6d7b9f23a775b3a0
younwony <wony9324@naver.com>
2022-07-19 21:32:24 +0900

    feat: BOJ - Bronze 4 - 15680. 연세대학교

---
+++ BOJ/_0_Bronze/Level_4/a15680_연세대학교.java
@@ -0,0 +1,13 @@
+package _0_Bronze.Level_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class a15680_연세대학교 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String input = bufferedReader.readLine();
+        System.out.println("0".equals(input) ? "YONSEI" : "Leading the Way to the Future");
+    }
+}
